bdabafa8108489e6977ae387f0ef7c79a5f52b3e
Alejandro Thon <alejandro.thon.j@gmail.com>
2024-02-17 18:38:48 -0300

    update

--- src/helpers/calculateValueRute.js
+++ src/helpers/calculateValueRute.js
@@ -1,16 +1,27 @@
 
 
-
+const valueDelivery = {
+    min: 2000,
+    mid: 2500,
+    max: 3000
+}
+const valueDistance = {
+    min: 1500,
+    mid: 2500,
+    max: 3050
+}
 
 export const calculateValueRute = (distance) => {
     let valueRute = 0;
     
-    (distance <= 1500) ?
-        valueRute = 1500
-        : distance <= 2500 ?
-            valueRute = 2000
-            : distance <= 3500 ? valueRute = 2500
-                : valueRute = 3000
+    
+    (distance <= valueDistance.min) ?
+        valueRute = valueDelivery.min
+        : distance <= valueDistance.mid ?
+            valueRute = valueDelivery.mid
+            : distance <= valueDistance.max ? valueRute = 2500
+                : valueRute = valueDelivery.mix
+
 
     return valueRute;
 }
