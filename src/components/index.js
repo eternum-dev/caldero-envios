86da3b6f85e46ed8095fa6469741256f76718e4b
Alejandro Thon <alejandro.thon.j@gmail.com>
2024-02-20 22:40:21 -0300

    add new rutes

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
 export * from './FormComponent';
 export * from './ResultRoute';
+export * from './CustomMarker';
+export * from './Direction';
+export * from './InputAutoComplete';
+export * from './styles/stylesMapDark';
