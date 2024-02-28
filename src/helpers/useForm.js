99c7ada531890e7e569930bc2cc2d8ee1504ae3a
Alejandro Thon <alejandro.thon.j@gmail.com>
2024-02-27 21:29:16 -0300

    create custom hook useForm

---
+++ src/helpers/useForm.js
@@ -0,0 +1,62 @@
+import {  useState } from "react";
+import { useMapContext } from "../context/MapContext";
+
+
+
+export const useForm = ({ inputRef }) => {
+
+    const { setLocalCoordinates,
+        setRepartidor,
+        setRenderState,
+        setDestination,
+        repartidor
+    } = useMapContext();
+
+    const [errorInput, setErrorInput] = useState(false);
+    const [errorRepartidor, setErrorRepartidor] = useState(false);
+
+    const onSelectLocal = (event) => {
+        const newCoordinates = JSON.parse(localStorage.getItem(event.target.value));
+        setLocalCoordinates(newCoordinates);
+    }
+
+    const onSelectRepartidor = (event) => {
+        const newRepartidor = localStorage.getItem(event.target.value);
+        setRepartidor(newRepartidor);
+    }
+
+
+    const onSubmitForm = (event) => {
+        event.preventDefault();
+
+
+        //validaciones 
+        if (inputRef.current.value.length <= 10) {
+            setErrorInput(true);
+            setTimeout(() => {
+                setErrorInput(false);
+            }, 600);
+            return;
+        }
+
+        if (repartidor === '') {
+            setErrorRepartidor(true);
+            setTimeout(() => {
+                setErrorRepartidor(false);
+            }, 600);
+            return;
+        }
+
+
+        setDestination(inputRef.current.value);
+        setRenderState(true);
+    }
+
+    return {
+        errorInput,
+        errorRepartidor,
+        onSelectLocal,
+        onSubmitForm,
+        onSelectRepartidor
+    }
+}
