a376b51fa7a53196d2e976200a15ffef8420e7b1
Alejandro Thon <alejandro.thon.j@gmail.com>
2024-02-27 21:12:31 -0300

    implement useMyDriectionService

--- src/components/Direction.jsx
+++ src/components/Direction.jsx
@@ -1,52 +1,10 @@
-import { useMap, useMapsLibrary } from '@vis.gl/react-google-maps';
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
-import { useMapContext } from '../context/MapContext';
+import { useMyDirectionService } from '../helpers/useMyDirectionService';
 
 
-export const Direction = ({localCordinates}) => {
-    const google = window.google;
-
-    const map = useMap(null);
-    const routerLibrary = useMapsLibrary('routes');
-
-    const [directionsService, setDirectionsService] = useState(null);
-    const [directionsRenderer, setDirectionRenderer] = useState(null);
-
-    const {  destination, setDataRoute, setRenderState } = useMapContext();
-
-
-    useEffect(() => {
-        if (!map || !routerLibrary) return;
-
-        setDirectionRenderer(new routerLibrary.DirectionsRenderer({ map }));
-        setDirectionsService(new routerLibrary.DirectionsService());
-
-    }, [map, routerLibrary]);
-
-    useEffect(() => {
-        if (!directionsService || !directionsRenderer) return;
-
-        directionsService.route({
-            origin: localCordinates,
-            destination: destination, // destination
-            travelMode: google.maps.TravelMode.DRIVING,
-            provideRouteAlternatives: true
-        }).then((response) => {
-            directionsRenderer.setDirections(response);
-            // directionsRenderer.;
-            setDataRoute(response.routes);
-        })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [directionsService, directionsRenderer]);
-
-    const resetMap = () => {
-        if (directionsRenderer) {
-            directionsRenderer.setMap(null);
-            setDataRoute([]);
-            setRenderState(false)
-        }
-    };
+export const Direction = ({ localCordinates }) => {
+    
+    const { resetMap } = useMyDirectionService({ localCordinates });
 
     return (
         <div className='directions'>
