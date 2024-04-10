038aeb901d800194c0cd95790af0b244ee9730ae
eternum-dev <alejandro.thon.j@gmail.com>
2024-04-09 23:10:21 -0400

    create new component

---
+++ src/components/ConfigButton.jsx
@@ -0,0 +1,17 @@
+
+
+export const ConfigButton = () => {
+    return (
+        <button
+            style={{
+                position: "fixed",
+                top: '20px',
+                right: '20px',
+                width: '100px',
+                background: '#ccc',
+                zIndex: 100
+            }}>
+            <img src="#" alt="config" />
+        </button>
+    )
+}
