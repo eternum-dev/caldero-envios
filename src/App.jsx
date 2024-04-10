0355e9ac1f3ab20c143c8e64b0d509e22a532063
eternum-dev <alejandro.thon.j@gmail.com>
2024-04-09 23:11:37 -0400

    add new component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { APIProvider } from '@vis.gl/react-google-maps';
 import { MapComponent } from './components/Map';
 import { MapProvider } from './context/MapContext';
+import { ConfigButton } from './components/ConfigButton';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <h1>caldero envios</h1>
       <APIProvider apiKey={API_KEY} libraries={['places']}>
         <MapProvider>
+          <ConfigButton />
           <MapComponent />
         </MapProvider>
       </APIProvider>
