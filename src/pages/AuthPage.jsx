aabc22ad5654848bda7ec0fa97f98137af959f48
eternum-dev <alejandro.thon.j@gmail.com>
2024-04-15 20:57:50 -0400

    create pages with authpage component

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,12 +1,45 @@
-import './authPage.css';
+import { useState } from 'react';
 
-
+import { Login } from '../components';
+import './authpage.css';
+import { Register } from '../components/Register';
 
 export const AuthPage = () => {
 
+    const [isRegistered, setIsRegistered] = useState(true);
+
+
     return (
-        <div>
-            <h2 style={{ color: 'white' }}>authpage</h2>
-        </div>
+        <div className='authpage'>
+            <div className='authpage__buttonbox'>
+                <button
+                    onClick={() => setIsRegistered(true)}
+                    className={`authpage__button btn-left ${!isRegistered && 'authpage__button--isActive'}`}
+                >
+                    login
+                </button>
+                <div className="dividerr"> </div>
+                <button
+                    onClick={() => setIsRegistered(false)}
+                    className={`authpage__button btn-right ${isRegistered && 'authpage__button--isActive'}`}
+                >
+                    sign-in
+                </button>
+            </div>
+
+
+            {
+                isRegistered ?
+                    (
+                        <Login />
+                    ) :
+                    (
+                        <Register />
+                    )
+            }
+
+
+
+        </div >
     )
 }
