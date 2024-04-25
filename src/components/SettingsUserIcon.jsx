3cbf256b3c8d04313a10aa3a5987be5d436e960b
eternum-dev <alejandro.thon.j@gmail.com>
2024-04-25 18:28:05 -0400

    Icons were created for user and system configuration

---
+++ src/components/SettingsUserIcon.jsx
@@ -0,0 +1,30 @@
+
+
+export const SettingsUserIcon = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            fill="none"
+            strokeWidth="1.4"
+            color="#ff8b8b"
+            viewBox="0 0 24 24"
+        >
+            <path
+                stroke="#ff8b8b"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2z"
+            ></path>
+            <path
+                stroke="#ff8b8b"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.271 18.346S6.5 15.5 12 15.5s7.73 2.846 7.73 2.846M12 12a3 3 0 100-6 3 3 0 000 6z"
+            ></path>
+        </svg>
+    );
+}
+
+export default SettingsUserIcon;
