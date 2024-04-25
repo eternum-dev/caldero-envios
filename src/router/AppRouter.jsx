7e54454f7c54d8eadc3e10dd6a037c49802c2fe2
eternum-dev <alejandro.thon.j@gmail.com>
2024-04-25 18:29:56 -0400

    add pages routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,42 +2,44 @@ import { useState } from "react"
 import { Navigate, Route, Routes } from "react-router-dom"
 import { AuthPage } from "../pages/AuthPage";
 import { MapPage } from "../pages/MapPage";
+import { ProtectedRoute } from "../components/ProtectedRoute";
 
 
 
 
 export const AppRouter = () => {
 
-    const [status, setStatus] = useState('not-authenticated');
+    const [status, setStatus] = useState(null);
+
 
     if (status === 'checking') {
         console.log(setStatus)
         return (
             <h2>...cargando</h2>
         )
-
     }
+
 
     return (
         <Routes>
-            {
-                status === 'not-authenticated' ?
-                    (
-                        <>
-                        {/* Public Routes */}
 
-                            <Route path="/auth/" element={<AuthPage />} />
-                            <Route path="/*" element={<Navigate to='/auth/' />} />
-                        </>
-                    ) :
-                    (
-                        <>
-                            {/* Private Routes */}
-                            <Route path="/" element={<MapPage />} />
-                            <Route path="/*" element={<Navigate to='/' />} />
-                        </>
-                    )
-            }
+
+            {/* Public Routes */}
+
+            <Route path="/auth/" element={<AuthPage />} />
+
+            {/* Private Routes */}
+            <Route path="/"
+                element={
+                    <ProtectedRoute>
+                        <MapPage />
+                    </ProtectedRoute>
+                }
+            >
+            </Route>
+
+            <Route path="/*" element={<Navigate to='/' />} />
+
 
         </Routes>
 
