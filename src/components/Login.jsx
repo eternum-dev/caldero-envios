3c3635f6545718c9423439cf96438f0e6815efe8
eternum-dev <alejandro.thon.j@gmail.com>
2024-04-25 18:31:54 -0400

    Added components for authentication page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,30 +1,80 @@
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 import { Divider, Hr, InputField } from "."
 import facebook from '../assets/icons/facebook.svg';
 import google from '../assets/icons/google.svg';
 import user from '../assets/icons/user-square.svg';
 import './login.css'
+import { useState } from "react";
+import { signIn, signInWithGoogle } from "../firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 
 
 export const Login = () => {
+
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [ErrorMessage, setErrorMessage] = useState('');
+    const [signingIn, setSigningIn] = useState(false);
+
+
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (!signingIn) {
+            setSigningIn(true);
+            await signIn(email, password)
+
+        }
+        navigate("/")
+    }
+
+    const onSignInGoogle = async (event) => {
+        event.preventDefault();
+        if (!signingIn) {
+            setSigningIn(true);
+            await signInWithGoogle()
+                .catch(err => {
+                    setErrorMessage(err);
+                    console.log(err);
+                    setSigningIn(false);
+                })
+
+        }
+        navigate("/")
+    }
     return (
         <div className="login authpage__container">
+
             <h3 className='login__title'>Hola de nuevo!</h3>
 
-            <form action="" className='authpage__form'>
+            <form action="" className='authpage__form' onSubmit={onSubmit}>
 
                 <Divider text='Inicia con' />
 
-                <InputField name='email' type='email' />
+                <InputField
+                    name='email'
+                    type='email'
+                    value={email}
+                    onChange={(event) => { setEmail(event.target.value) }}
+                />
 
-                <InputField name='password' type='password' />
+                <InputField
+                    name='password'
+                    type='password'
+                    value={password}
+                    onChange={(event) => { setPassword(event.target.value) }} />
 
                 <div className="login__submitbox" >
                     <Hr justify='start' />
-                    <button>enviar</button>
+                    <button type="submit" style={{ padding: '.8rem  1.2rem', color: 'var(--white)', border: '1px solid var(--white)', borderRadius:'5px' }} >enviar</button>
                     <Hr />
                 </div>
-
+                {
+                    ErrorMessage && ErrorMessage
+                }
                 <div className='login__alterbox'>
 
                     <Divider text='O puedes usar' />
@@ -36,24 +86,21 @@ export const Login = () => {
                             gridTemplateRows: '1fr'
                         }}
                     >
-                        <Link to='https://google.com'
-                            target='_blank'>
+                        <button onClick={(event) => onSignInGoogle(event)}>
                             <img src={google} alt="" style={{ width: '60px' }} />
-                        </Link>
+                        </button>
+
 
                         <Hr size='3px' justify='center' />
 
-                        <Link to='https://google.com'
-                            target='_blank'>
-                            <img src={facebook} alt="" style={{ width: '60px' }} />
-                        </Link>
+
+                        <img src={facebook} alt="" style={{ width: '60px' }} />
+
 
                         <Hr size='3px' justify='center' />
 
-                        <Link to='https://google.com'
-                            target='_blank'>
-                            <img src={user} alt="" style={{ width: '60px' }} />
-                        </Link>
+                        <img src={user} alt="" style={{ width: '60px' }} />
+
                     </div>
                 </div>
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,31 +1,92 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { Divider, Hr, InputField } from "."
 import facebook from '../assets/icons/facebook.svg';
 import google from '../assets/icons/google.svg';
 import user from '../assets/icons/user-square.svg';
 import './register.css'
+import { useState } from "react";
+import { createUser, signIn, signInWithGoogle } from "../firebase/auth";
 
 
 export const Register = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
+    const [ErrorMessage, setErrorMessage] = useState('');
+    const [signingIn, setSigningIn] = useState(false);
+
+    const navigate = useNavigate();
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (!signingIn) {
+            setSigningIn(true);
+            await createUser(email, password)
+                .catch(err => console.log(err));
+
+            await signIn(email, password)
+                .then(navigate("/"));
+        }
+
+
+    }
+
+    const onSignInGoogle = async (event) => {
+        event.preventDefault();
+        if (!signingIn) {
+            setSigningIn(true);
+            signInWithGoogle()
+                .then(console.log('hola'), navigate("/"))
+                .catch(err => {
+                    setErrorMessage(err);
+                    setSigningIn(false);
+                });
+        }
+    }
+
     return (
         <div className="register authpage__container">
             <h3 className='register__title'>construyamos un mapa juntos!</h3>
 
-            <form action="" className='authpage__form'>
+            <form action="" className='authpage__form' onSubmit={onSubmit}>
 
                 <Divider text='Registrate con' />
 
-                <InputField name='name' type='text' />
-                <InputField name='email' type='email' />
+                <InputField
+                    name='name'
+                    type='text'
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                />
+                <InputField
+                    name='email'
+                    type='email'
+                    value={email}
+                    onChange={(event) => { setEmail(event.target.value) }}
+                />
 
-                <InputField name='password' type='password' />
-                <InputField name='repeatPassword' type='password' />
+                <InputField
+                    name='password'
+                    type='password'
+                    value={password}
+                    onChange={(event) => { setPassword(event.target.value) }}
+                />
+                <InputField
+                    name='repeatPassword'
+                    type='password'
+                    value={repeatPassword}
+                    onChange={(event) => setRepeatPassword(event.target.value)}
+                />
 
                 <div className="register__submitbox" >
                     <Hr justify='start' />
                     <button>Registrarme</button>
                     <Hr />
                 </div>
+
+                {ErrorMessage && <h3 style={{ color: 'red', background: '#ccc' }}>{ErrorMessage}</h3>}
 
                 <div className='register__alterbox'>
 
@@ -38,10 +99,11 @@ export const Register = () => {
                             gridTemplateRows: '1fr'
                         }}
                     >
-                        <Link to='https://google.com'
-                            target='_blank'>
+                        <button onClick={(event) => onSignInGoogle(event)}>
                             <img src={google} alt="" style={{ width: '60px' }} />
-                        </Link>
+                        </button>
+
+
 
                         <Hr size='3px' justify='center' />
 
@@ -64,3 +126,5 @@ export const Register = () => {
         </div>
     )
 }
+
+
