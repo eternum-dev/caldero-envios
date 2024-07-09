7431c263eccd34a5e3d5c1e7e3ec1b1ec2b07053
eternum-dev <alejandro.thon.j@gmail.com>
2024-07-09 16:04:14 -0400

    refactor: integrate user authentication and local data fetching in MapProvider
    - Added user context to fetch local data.
    - Renamed 'renderState' to 'renderRoute' for better clarity and consistency.
    - Introduced 'local' state to store local information.
    - Added useEffect to fetch local data on user change.

--- src/context/map/MapProvider.jsx
+++ src/context/map/MapProvider.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { MapContext } from './MapContext';
+import { getLocal } from '../../helpers/getLocal';
+import { AuthContext } from '../auth/AuthContext';
 
 const initialCoords = { lat: -39.83823263400049, lng: -73.21039410495716 };
 
@@ -8,8 +10,25 @@ export const MapProvider = ({ children }) => {
     const [localCoordinates, setLocalCoordinates] = useState(initialCoords);
     const [repartidor, setRepartidor] = useState('');
     const [destination, setDestination] = useState('');
-    const [renderState, setRenderState] = useState(false);
+    const [renderRoute, setRenderRoute] = useState(false);
     const [dataRoute, setDataRoute] = useState([]);
+    const [local, setLocal] = useState(null);
+
+    const { user } = useContext(AuthContext)
+
+    useEffect(() => {
+        const fetchLocal = async () => {
+            try {
+                const localData = await getLocal();
+
+                setLocal(localData);
+            } catch (error) {
+                throw new Error(error);
+            }
+        };
+        fetchLocal();
+    }, [user]);
+
 
     const contextValue = {
         localCoordinates,
@@ -18,13 +37,15 @@ export const MapProvider = ({ children }) => {
         setRepartidor,
         destination,
         setDestination,
-        renderState,
-        setRenderState,
+        renderRoute,
+        setRenderRoute,
         dataRoute,
-        setDataRoute
+        setDataRoute,
+        local,
+        setLocal
     };
 
-    // TODO: dispach
+
 
     return (
         <MapContext.Provider value={contextValue}>
