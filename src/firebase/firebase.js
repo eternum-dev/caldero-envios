6bdeb79a7adc610220bd725ce0decaf9aa05277a
eternum-dev <alejandro.thon.j@gmail.com>
2024-07-09 15:56:57 -0400

    feat(firebase): update Firebase configuration and add Firestore integration
    - Updated Firebase configuration to use environment variables:
      - VITE_FIREBASE_API_KEY
      - VITE_FIREBASE_AUTHDOMAIN
      - VITE_FIREBASE_PROJECTID
      - VITE_FIREBASE_STORAGEBUCKET
      - VITE_FIREBASE_MESSAGINGSENDERID
      - VITE_FIREBASE_APPID
    - Added Firestore integration:
      - Imported and initialized Firestore with 'getFirestore'
      - Exported Firestore instance

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,23 +1,33 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
+//*  Firebase configuration
+
+//* Environment Variables
+
+const apiKey = import.meta.env.VITE_FIREBASE_API_KEY ;
+const authDomain = import.meta.env.VITE_FIREBASE_AUTHDOMAIN;
+const projectId = import.meta.env.VITE_FIREBASE_PROJECTID;
+const storageBucket = import.meta.env.VITE_FIREBASE_STORAGEBUCKET;
+const messagingSenderId = import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID;
+const appId = import.meta.env.VITE_FIREBASE_APPID;
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCBSZnarpdKVeGh5sikbxhDXElR8PRvvc8",
-    authDomain: "caldero-envios.firebaseapp.com",
-    projectId: "caldero-envios",
-    storageBucket: "caldero-envios.appspot.com",
-    messagingSenderId: "455012053818",
-    appId: "1:455012053818:web:0dcf6467bd0406561bd603"
+    apiKey,
+    authDomain,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId
 };
 
-// Initialize Firebase
+//* Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-
-export { app, auth };
+export { app, auth, db };
