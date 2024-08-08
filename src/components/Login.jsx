56b0d58387d88b67b4d2dc29ee0e019aa058ba9e
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-08 11:16:57 -0400

    style(login): format files using Prettier
    - Applied Prettier to login.css and Login.jsx to improve code formatting and consistency.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,101 +1,97 @@
 // import { Link } from "react-router-dom"
-import { Divider, Hr, InputField } from "."
-import google from '../assets/icons/google.svg';
-import './login.css'
+import { Divider, Hr, InputField } from ".";
+import google from "../assets/icons/google.svg";
+import "./login.css";
 import { useState } from "react";
 import { signIn, signInWithGoogle } from "../firebase/auth";
 import { useNavigate } from "react-router-dom";
 
-
-
 export const Login = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [ErrorMessage, setErrorMessage] = useState("");
+  const [signingIn, setSigningIn] = useState(false);
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [ErrorMessage, setErrorMessage] = useState('');
-    const [signingIn, setSigningIn] = useState(false);
-
-
-
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        if (!signingIn) {
-            setSigningIn(true);
-            await signIn(email, password)
-
-        }
-        navigate("/")
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (!signingIn) {
+      setSigningIn(true);
+      await signIn(email, password);
     }
+    navigate("/");
+  };
 
-    const onSignInGoogle = async (event) => {
-        event.preventDefault();
-        if (!signingIn) {
-            setSigningIn(true);
-            await signInWithGoogle()
-                .catch(err => {
-                    setErrorMessage(err);
-                    console.log(err);
-                    setSigningIn(false);
-                })
-
-        }
-        navigate("/")
+  const onSignInGoogle = async (event) => {
+    event.preventDefault();
+    if (!signingIn) {
+      setSigningIn(true);
+      await signInWithGoogle().catch((err) => {
+        setErrorMessage(err);
+        console.log(err);
+        setSigningIn(false);
+      });
     }
-    return (
-        <div className="login authpage__container">
+    navigate("/");
+  };
 
-            <h3 className='login__title'>Hola de nuevo!</h3>
+  return (
+    <div className="login authpage__container">
+      <h3 className="login__title">Hola de nuevo!</h3>
 
-            <form action="" className='authpage__form' onSubmit={onSubmit}>
+      <form action="" className="authpage__form" onSubmit={onSubmit}>
+        <Divider text="Inicia con" />
 
-                <Divider text='Inicia con' />
+        <InputField
+          name="email"
+          type="email"
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
 
-                <InputField
-                    name='email'
-                    type='email'
-                    value={email}
-                    onChange={(event) => { setEmail(event.target.value) }}
-                />
+        <InputField
+          name="password"
+          type="password"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
 
-                <InputField
-                    name='password'
-                    type='password'
-                    value={password}
-                    onChange={(event) => { setPassword(event.target.value) }} />
-
-                <div className="login__submitbox" >
-                    <Hr justify='start' />
-                    <button type="submit" style={{ padding: '.8rem  1.2rem', color: 'var(--white)', border: '1px solid var(--white)', borderRadius: '5px' }} >enviar</button>
-                    <Hr />
-                </div>
-                {
-                    ErrorMessage && ErrorMessage
-                }
-                <div className='login__alterbox'>
-
-                    <Divider text='O puedes usar' />
-
-                    <div className='login__links'
-
-                    >
-                        <Hr size='3px' justify='center' />
-                        <button onClick={(event) => onSignInGoogle(event)}>
-                            <img src={google} alt="" style={{ width: '60px' }} />
-                        </button>
-
-
-                        <Hr size='3px' justify='center' />
-
-                    </div>
-                </div>
-
-            </form>
-            <img src="#" alt="login image" className='login__image' style={{
-                 background: '#ccc',
-                margin: ' 0 2rem'
-            }} />
+        <div className="login__submitbox">
+          <Hr justify="start" />
+          <button className="loggin__submit" type="submit">
+            enviar
+          </button>
+          <Hr />
         </div>
-    )
-}
+        {ErrorMessage && ErrorMessage}
+        <div className="login__alterbox">
+          <Divider text="O puedes usar" />
+
+          <div className="login__links">
+            <Hr size="3px" justify="center" />
+            <button onClick={(event) => onSignInGoogle(event)}>
+                {/* pasar a icon jsx */}
+              <img src={google} alt="" style={{ width: "60px" }} />
+            </button>
+
+            <Hr size="3px" justify="center" />
+          </div>
+        </div>
+      </form>
+      <img
+        src="#"
+        alt="login image"
+        className="login__image"
+        style={{
+          background: "#ccc",
+          margin: " 0 2rem",
+        }}
+      />
+    </div>
+  );
+};
