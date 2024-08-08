33f1cf331c69fbb9d65b26f9fafb1280f9d03ec4
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-08 11:18:22 -0400

    style(form): format files using Prettier
    - Applied Prettier to formComponent.css and FormComponent.jsx to improve code formatting and consistency.

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,74 +1,78 @@
-import { useContext, useRef } from 'react';
-import './formComponent.css';
-import { InputAutoComplete } from './InputAutoComplete';
+import { useContext, useRef } from "react";
+import "./formComponent.css";
+import { InputAutoComplete } from "./InputAutoComplete";
 // import { MapContext } from '../context/MapContext';
-import { useForm } from '../helpers/useForm';
-import { MapContext } from '../context/map/MapContext';
-
-
-
+import { useForm } from "../helpers/useForm";
+import { MapContext } from "../context/map/MapContext";
 
 export const FormComponent = () => {
+  //
+  const { localCoordinates, local } = useContext(MapContext);
+  const inputRef = useRef(null);
+  const {
+    errorInput,
+    errorRepartidor,
+    onSelectLocal,
+    onSelectRepartidor,
+    onSubmitForm,
+  } = useForm({ inputRef });
 
-    const { localCoordinates, repartidor } = useContext(MapContext);
+  if (!local) {
+    return <form className="formComponent"></form>;
+  }
 
-    const inputRef = useRef(null);
+  const { repartidores, locales } = local;
 
-    const {
-        errorInput,
-        errorRepartidor,
-        onSelectLocal,
-        onSelectRepartidor,
-        onSubmitForm } = useForm({ inputRef });
+  return (
+    <form className="formComponent">
+      <label htmlFor="direccion" className="formComponent__direccion">
+        Direccion
+        <InputAutoComplete inputRef={inputRef} errorInput={errorInput} />
+      </label>
+      <label htmlFor="local" className="formComponent__local">
+        Envios
+        <select
+          name="local"
+          className={`formComponent__select`}
+          onChange={onSelectLocal}
+          value={localCoordinates}
+        >
+          <option value="seleccionar">Selecciona un local</option>
+          {locales.map((local) => (
+            <option value={local} key={local}>
+              {local}
+            </option>
+          ))}
+        </select>
+      </label>
+      <label htmlFor="repartidor" className="formComponent__repartidor">
+        Repartidor
+        <select
+          name="repartidor"
+          className={`formComponent__select ${
+            errorRepartidor ? "error-animation" : ""
+          }`}
+          value={"repartidor"}
+          onChange={onSelectRepartidor}
+        >
+          <option value="seleccionar"> selecciona un repartidor </option>
+          {repartidores.map((deliman, index) => {
+            return (
+              <option key={index} value={`${deliman.nombre}`}>
+                {deliman.nombre}
+              </option>
+            );
+          })}
+        </select>
+      </label>
 
-
-    return (
-        <form className='formComponent'>
-            <label htmlFor="direccion"
-                className='formComponent__direccion'
-            >
-                Direccion
-                <InputAutoComplete
-                    inputRef={inputRef}
-                    errorInput={errorInput} />
-
-            </label>
-            <label htmlFor="local"
-                className='formComponent__local'
-            >
-                Envios
-                <select name='local'
-                    className={`formComponent__select`}
-                    onChange={onSelectLocal}
-                    value={localCoordinates}
-                >
-                    <option value='caldero de la bruja'>caldero de la bruja</option>
-                    <option value='caldero del brujo'>caldero del brujo</option>
-                </select>
-            </label>
-            <label htmlFor="repartidor"
-                className='formComponent__repartidor'
-            >
-                Repartidor
-                <select name='repartidor'
-                    className={`formComponent__select ${errorRepartidor ? 'error-animation' : ''}`}
-                    value={repartidor}
-                    onChange={onSelectRepartidor}
-                >
-                    <option value='seleccionar'> selecciona un repartidor </option>
-                    <option value='erwin thon'> erwin thon</option>
-                    <option value='ferrnando alonso'>ferrnando alonso</option>
-                </select>
-            </label>
-
-            <button
-                type='submit'
-                className='formComponent__button'
-                onClick={onSubmitForm}
-                style={{padding: '.8rem 1.2rem', border: '1px solid  var(--white)', color: 'var(--white)', borderRadius:'5px'}}
-            >
-                calcular
-            </button>
-        </form >
-    )
-}
+      <button
+        type="submit"
+        className="formComponent__button"
+        onClick={onSubmitForm}
+      >
+        calcular
+      </button>
+    </form>
+  );
+};
