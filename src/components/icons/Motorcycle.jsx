40dd71ad380afd385e2c65e4462ee369bcd65577
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-10 18:01:11 -0400

    feat(components): add Motorcycle component with SVG ico

---
+++ src/components/icons/Motorcycle.jsx
@@ -0,0 +1,26 @@
+export const Motorcycle = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      strokeWidth="1.5"
+      color="#fafafa"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#fafafa"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M5 19a4 4 0 100-8 4 4 0 000 8zM19 15l-3-9 1-1"
+      ></path>
+      <path
+        stroke="#fafafa"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16 8.5h-4.5l-4.5 3M5.5 15.5H12l1-2.5 3.5-4.5M8.5 10c-2-1.5-5-1.5-7 0M19 19a4 4 0 100-8 4 4 0 000 8z"
+      ></path>
+    </svg>
+  );
+};
