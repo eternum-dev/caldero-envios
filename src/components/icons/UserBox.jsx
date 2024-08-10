551c2a85848c47e675565eca34a5ba11f19d9246
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-10 18:37:20 -0400

    refactor(UserBox): remove user-box.svg and convert to JSX component

---
+++ src/components/icons/UserBox.jsx
@@ -0,0 +1,31 @@
+export const UserBox = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      strokeWidth="1.4"
+      color="#fee"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#fee"
+        strokeLinecap="round"
+        d="M7 18v-1a5 5 0 015-5v0a5 5 0 015 5v1"
+      ></path>
+      <path
+        stroke="#fee"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 12a3 3 0 100-6 3 3 0 000 6z"
+      ></path>
+      <path
+        stroke="#fee"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 3.6v16.8a.6.6 0 01-.6.6H3.6a.6.6 0 01-.6-.6V3.6a.6.6 0 01.6-.6h16.8a.6.6 0 01.6.6z"
+      ></path>
+    </svg>
+  );
+};
