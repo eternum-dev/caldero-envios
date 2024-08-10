5eeb1a69c8d04fecd3f5c1db99bcdb986b703817
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-10 18:39:08 -0400

    refactor(SavePassword): remove save-password.svg and convert to JSX component

---
+++ src/components/icons/SavePassword.jsx
@@ -0,0 +1,26 @@
+export const SavePassword = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      strokeWidth="1.4"
+      color="#fee"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#fee"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 6v5a9 9 0 11-18 0V6a2 2 0 012-2h14a2 2 0 012 2z"
+      ></path>
+      <path
+        stroke="#fee"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M8 10l4 4 4-4"
+      ></path>
+    </svg>
+  );
+};
