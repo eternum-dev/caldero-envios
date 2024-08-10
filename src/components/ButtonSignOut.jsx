52e751958e8f32d3646427667e9abfaf5b39602f
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-10 18:03:52 -0400

    refactor(ButtonSignOut): reorganize imports and separate styles into buttonSignOut.css

--- src/components/ButtonSignOut.jsx
+++ src/components/ButtonSignOut.jsx
@@ -1,28 +1,18 @@
-import { Hr } from "./Hr";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth/AuthContext";
 import PropTypes from "prop-types";
 import { signOut } from "../firebase/auth";
+import { Hr } from "./Hr";
+import "./buttonSignOut.css"
 
 export const ButtonSignOut = ({ setModal }) => {
   const { setUser } = useContext(AuthContext);
 
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "1fr auto 1fr",
-        alignItems: "center",
-      }}
-    >
+    <div className="buttonsignout">
       <Hr />
       <button
-        style={{
-          padding: ".8rem  1.2rem",
-          color: "var(--white)",
-          border: "1px solid var(--white)",
-          borderRadius: "5px",
-        }}
+        className="buttonsignout__btn"
         onClick={() => {
           setModal && setModal((prev) => !prev);
           signOut();
