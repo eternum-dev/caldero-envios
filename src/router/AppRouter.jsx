53fa6fa6fa615d04e9d63dde531c6b110cf91440
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-12 22:23:05 -0400

    refactor(router): update routing structure
    - Add nested routes for settings pages (Profile, Password, Deliveryman, Branches)
    - Update routing to use Outlet in ProtectedRoute
    - Ensure private routes are protected by authentication

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,41 +1,34 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom";
 import { AuthPage } from "../pages/auth/AuthPage";
 import { MapPage } from "../pages/map/MapPage";
 import { ProtectedRoute } from "../components/ProtectedRoute";
 import { HomePage } from "../pages/home/HomePage";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth/AuthContext";
-
-
-
+import { Profile, Branches, Deliveryman, Password } from "../pages/setting";
 
 export const AppRouter = () => {
+  const { user } = useContext(AuthContext);
 
+  return (
+    <Routes>
+      {/* Public Routes  */}
+      <Route path="/home" element={<HomePage />} />
+      <Route
+        path="/auth"
+        element={!user ? <AuthPage /> : <Navigate to="/" />}
+      />
 
-    const { user } = useContext(AuthContext);
+      {/* Private Routes */}
+      <Route element={<ProtectedRoute></ProtectedRoute>}>
+        <Route path="/" element={<MapPage />} />
+        <Route path="/setting/perfil" element={<Profile />} />
+        <Route path="/setting/contraseña" element={<Password />} />
+        <Route path="/setting/repartidor" element={<Deliveryman />} />
+        <Route path="/setting/local" element={<Branches />} />
+      </Route>
 
-    return (
-        <Routes>
-
-
-            {/* Public Routes  */}
-
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/auth" element={!user ? <AuthPage /> : <Navigate to='/' />} />
-
-            {/* Private Routes */}
-            <Route path="/"
-                element={
-                    <ProtectedRoute>
-                        <MapPage />
-                    </ProtectedRoute>
-                }
-            >
-            </Route>
-
-
-            <Route path="/*" element={<Navigate to='/' />} />
-        </Routes>
-
-    )
-}
+      <Route path="/*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
