42ef6a909d2820738684f4beb5d08f10eaa648e7
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-12 22:25:33 -0400

    feat(profile): add Profile page component and styles
    - Implement Profile component with form for editing user information
    - Add profile image section with edit and delete buttons
    - Utilize InputField component for user input fields (name, email)
    - Style Profile component with custom CSS for layout and design

---
+++ src/pages/setting/Profile.jsx
@@ -0,0 +1,30 @@
+import { Hr } from "../../components/Hr";
+import { InputField } from "../../components/InputField";
+import "./profile.css";
+
+export const Profile = () => {
+  return (
+    <div className="profile">
+      <h1>Settings Profile</h1>
+      <Hr justify="center"/>
+      <form action="">
+        <div className="profile__picture">
+          <img className="profile__img" src="#" alt="" />
+          <div className="profile__buttonbox">
+            <button className="profile__button">
+              {/* agregar icon */}
+              editar
+            </button>
+            <button className="profile__button">
+              {/* agregar icon */}
+              eliminar
+            </button>
+          </div>
+        </div>
+        <InputField name={"nombre"} > </InputField>
+        <InputField name={"correo"} type="email" > </InputField>
+        <button className="profile__button profile__button--submit">guardar</button>
+      </form>
+    </div>
+  );
+};
