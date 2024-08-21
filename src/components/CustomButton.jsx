b98fba12a0a13a805483531d219e44e6945de201
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-21 10:31:36 -0400

    feat(CustomButton): add optional size prop to control button width and centering

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,9 +1,13 @@
 import PropTypes from "prop-types";
 import "./customButton.css";
 
-export const CustomButton = ({ children, onClick }) => {
+export const CustomButton = ({ children, onClick, size = "none" }) => {
   return (
-    <button className="custom-button" onClick={onClick}>
+    <button
+      className="custom-button"
+      onClick={onClick}
+      style={{ maxWidth: size, margin: `${size !== "none" ? "auto" : ""} ` }}
+    >
       {children}
     </button>
   );
@@ -12,4 +16,5 @@ export const CustomButton = ({ children, onClick }) => {
 CustomButton.propTypes = {
   children: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
+  size: PropTypes.string,
 };
