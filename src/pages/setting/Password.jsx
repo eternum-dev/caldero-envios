ee82b5f3ff5c0f5ad370bd7b3457dafc91ba22f9
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-26 21:30:19 -0400

    feat(password): add change password functionality with reauthentication
    - Implemented state management for currentPassword, newPassword, and repeatPassword.
    - Added error and success messages for better user feedback.
    - Updated handleChangePassword function to include reauthentication with Firebase.
    - Modified InputField components to handle value and onChange props.
    - Updated CustomButton component to use type='submit' for form submission.
    - Added temporary placeholder for error and success messages (to be replaced with a modal in the future).

--- src/pages/setting/Password.jsx
+++ src/pages/setting/Password.jsx
@@ -1,24 +1,78 @@
+import { useState } from "react";
 import { CustomButton } from "../../components/CustomButton";
 import { Hr } from "../../components/Hr";
 import { InputField } from "../../components/InputField";
+import { EmailAuthProvider, reauthenticateWithCredential } from "firebase/auth";
+import { auth } from "../../firebase/firebase";
+import { changePassword } from "../../firebase/auth";
 import "./password.css";
 
 export const Password = () => {
-  const handleClick = (event) => {
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+
+  const handleChangePassword = async (event) => {
     event.preventDefault();
-    console.log("clave guardada");
+    // Reautenticación del usuario
+    const user = auth.currentUser;
+    console.log(user.email);
+    const credential = EmailAuthProvider.credential(
+      user.email,
+      currentPassword
+    );
+
+    try {
+      await reauthenticateWithCredential(user, credential);
+      // Si la reautenticación fue exitosa, se procede a cambiar la contraseña
+      await changePassword(user, newPassword);
+      setSuccess("Contraseña actualizada  con exito!");
+      setError("");
+    } catch (error) {
+      setError("Error: " + error.message);
+    }
+  };
+
+  const handleChangeInput = (event, setState) => {
+    setState(event.target.value);
   };
 
   return (
     <div className="password">
-      <h1>Password</h1>
+      <h1>Actualizar contraseña</h1>
       <Hr />
-      <form action="">
-        <InputField name="Contraseña Actual" type="password" />
-        <InputField name="Nueva Contraseña" type="password" />
-        <InputField name="Repite la Contraseña" type="password" />
-        <CustomButton onClick={handleClick}>Guardar</CustomButton>
+      <form action="" onSubmit={handleChangePassword}>
+        <InputField
+          name="Contraseña Actual"
+          type="password"
+          value={currentPassword}
+          onChange={(event) => handleChangeInput(event, setCurrentPassword)}
+        />
+
+        <InputField
+          name="Nueva Contraseña"
+          type="password"
+          value={newPassword}
+          onChange={(event) => handleChangeInput(event, setNewPassword)}
+        />
+
+        <InputField
+          name="Repite la Contraseña"
+          type="password"
+          value={repeatPassword}
+          onChange={(event) => handleChangeInput(event, setRepeatPassword)}
+        />
+
+        <CustomButton type={"submit"}>Guardar</CustomButton>
       </form>
+
+      {/* TODO: cambiar por un loading 
+        mostrar atraves de un modal */}
+      <p style={{ color: "white", textAlign: "center" }}>
+        {error ? error : success}
+      </p>
     </div>
   );
 };
