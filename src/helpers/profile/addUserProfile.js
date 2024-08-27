44b744f40f0bc5404cdc71baa2b00db17e84a71b
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-26 20:54:20 -0400

    feat(helpers): add addUserProfile function to save user profile data

---
+++ src/helpers/profile/addUserProfile.js
@@ -0,0 +1,13 @@
+import { doc, setDoc } from "firebase/firestore";
+import { auth, db } from "../../firebase/firebase";
+
+export const addUserProfile = async (profileData) => {
+
+  try {
+    const user = auth.currentUser;
+    await setDoc(doc(db, "usuarios", user.uid), profileData);
+    console.log("usuario añadido con exito");
+  } catch (error) {
+    console.error("Error al añadir el perfil: ", error);
+  }
+};
