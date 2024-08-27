1c8eab22537bd98e21bb03bd529c0e436369868a
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-26 20:53:39 -0400

    refactor(firebase): apply Prettier formatting and add Firebase Storage initialization

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,33 +1,32 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-
-
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 //*  Firebase configuration
 
 //* Environment Variables
 
-const apiKey = import.meta.env.VITE_FIREBASE_API_KEY ;
+const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
 const authDomain = import.meta.env.VITE_FIREBASE_AUTHDOMAIN;
 const projectId = import.meta.env.VITE_FIREBASE_PROJECTID;
 const storageBucket = import.meta.env.VITE_FIREBASE_STORAGEBUCKET;
 const messagingSenderId = import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID;
 const appId = import.meta.env.VITE_FIREBASE_APPID;
 
-
 const firebaseConfig = {
-    apiKey,
-    authDomain,
-    projectId,
-    storageBucket,
-    messagingSenderId,
-    appId
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 };
 
 //* Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { app, auth, db };
+export { app, auth, db, storage };
