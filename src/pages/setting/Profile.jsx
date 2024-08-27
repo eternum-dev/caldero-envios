95050ccac50441aaf5b27a510807170ca8db1b64
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-26 21:37:09 -0400

    feat(profile): enhance profile management with image upload and profile update
    - Added state management for profile data, image preview, and modal visibility.
    - Implemented profile fetching and updating with Firebase.
    - Added image upload functionality with preview and modal for selection.
    - Introduced DisplayInput component for dynamic profile fields.
    - Updated form to handle profile updates and image changes.

--- src/pages/setting/Profile.jsx
+++ src/pages/setting/Profile.jsx
@@ -1,32 +1,105 @@
+import { useEffect, useState } from "react";
 import { CustomButton } from "../../components/CustomButton";
 import { Hr } from "../../components/Hr";
-import { InputField } from "../../components/InputField";
+import { getUserProfile } from "../../helpers/profile/getUserProfile";
+import { updateProfile } from "../../helpers/profile/updateUserprofile";
 import "./profile.css";
+import { DisplayInput } from "../../components/DisplayInput";
+import { Modal } from "../../components/Modal";
 
 export const Profile = () => {
+  const [profile, setProfile] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [picture, setPicture] = useState(null);
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const resp = await getUserProfile();
+        setProfile(resp);
+      } catch (error) {
+        throw new Error("this is error: ", error);
+      }
+    };
+
+    getUser();
+  }, []);
+
+  if (!profile) return;
+
+  const updateUserProfile = (event) => {
+    event.preventDefault();
+    const { name, email, profilePicture } = profile;
+
+    updateProfile(name, email, profilePicture);
+  };
+
+  const toogleModal = (id, event) => {
+    event.preventDefault();
+    setShowModal((prev) => (prev === id ? null : id));
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPicture(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="profile">
       <h1>Settings Profile</h1>
       <Hr justify="center" />
       <form action="">
         <div className="profile__picture">
-          <img className="profile__img" src="#" alt="" />
-          <div className="profile__buttonbox">
-            <button className="profile__button">
-              {/* agregar icon */}
-              editar
-            </button>
-            <button className="profile__button">
-              {/* agregar icon */}
-              eliminar
-            </button>
-          </div>
+          <img
+            className="profile__img"
+            src={`${!picture ? profile.profilePicture : picture}`}
+            alt="otro"
+          />
+          <Modal
+            showModal={showModal === 1}
+            toggleModal={(event) => toogleModal(1, event)}
+            triggerContent={"aaslñdasd"}
+          >
+            <h4>seleccionar foto</h4>
+            <div className="profile__modal">
+              <input
+                type="file"
+                className="profile__input"
+                accept="image/*"
+                onChange={handleImageChange}
+              />
+              <CustomButton
+                onClick={(event) => {
+                  event.preventDefault();
+                  setProfile((prev) => {
+                    return { ...prev, profilePicture: picture };
+                  });
+                  setShowModal(false);
+                }}
+              >
+                guardar
+              </CustomButton>
+            </div>
+          </Modal>
         </div>
-        <InputField name={"nombre"}> </InputField>
-        <InputField name={"correo"} type="email">
-          {" "}
-        </InputField>
-        <CustomButton>Guardar</CustomButton>
+        {Object.keys(profile).map(
+          (key) =>
+            key !== "profilePicture" && (
+              <DisplayInput
+                key={key}
+                value={profile[key]}
+                setInput={setProfile}
+                fieldName={key}
+              />
+            )
+        )}
+        <CustomButton onClick={updateUserProfile}>Guardar</CustomButton>
       </form>
     </div>
   );
