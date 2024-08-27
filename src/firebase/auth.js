f14fac170f55cf002175a9fe284b9b226ff9ed58
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-26 20:51:05 -0400

    refactor(helpers): apply Prettier formatting and add signOut and changePassword functionalities to auth module

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,26 +1,29 @@
 import {
-    GoogleAuthProvider,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signInWithPopup,
-} from "firebase/auth"
-import { auth } from "./firebase"
-
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  updatePassword,
+} from "firebase/auth";
+import { auth } from "./firebase";
 
 export const createUser = async (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password);
-}
+  return createUserWithEmailAndPassword(auth, email, password);
+};
 
 export const signIn = async (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password);
-}
-
+  return signInWithEmailAndPassword(auth, email, password);
+};
 
 export const signInWithGoogle = () => {
-    const googleProvider = new GoogleAuthProvider();
-    return signInWithPopup(auth, googleProvider);
-}
+  const googleProvider = new GoogleAuthProvider();
+  return signInWithPopup(auth, googleProvider);
+};
 
 export const signOut = () => {
-    return auth.signOut();
-}
+  return auth.signOut();
+};
+
+export const changePassword = (user, newPassword) => {
+  return updatePassword(user, newPassword);
+};
