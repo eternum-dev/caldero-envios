86aa909a14f1a1a6ce6d30a53ddde985f9f29a12
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-27 22:22:47 -0400

    feat(deliveryman): add functionality to update delivery input values
    - Added `updateInput` function to update simple fields like name and phone.
    - Added `updateRouteValue` function to handle updates for nested delivery route values.
    - Integrated `setInputValue` into `DisplayInput` component to manage state updates.
    - Improved the `toggleModal` function for better modal management.
    - Updated the addNewDelivery function to allow adding new delivery entries.

--- src/pages/setting/Deliveryman.jsx
+++ src/pages/setting/Deliveryman.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState,useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 import { MapContext } from "../../context/map/MapContext";
 import { Hr } from "../../components/Hr";
 import { DisplayInput } from "../../components/DisplayInput";
@@ -38,9 +38,29 @@ export const Deliveryman = () => {
       ];
     });
   };
-  
-  console.log(delivery);
 
+  const updateInput = (newValue, fieldName, index) => {
+    setDelivery((prev) =>
+      prev.map((item, i) =>
+        i === index ? { ...item, [fieldName]: newValue } : item
+      )
+    );
+  };
+
+  const updateRouteValue = (newValue, fieldName, index, currentItem) => {
+    console.log({ newValue, fieldName, index, currentItem });
+    setDelivery((prev) =>
+      prev.map((item, i) =>
+        i === index
+          ? {
+              ...item,
+              [currentItem]: { ...item[currentItem], [fieldName]: newValue },
+            }
+          : item
+      )
+    );
+  };
+  console.log(delivery);
   return (
     <div className="delivery">
       <h1>Deliveryman</h1>
@@ -52,56 +72,96 @@ export const Deliveryman = () => {
           <span>valores</span>
         </div>
         {delivery &&
-          delivery.map((mobil, index) => (
-            <div className="delivery__row" key={index}>
-              <DisplayInput value={mobil.nombre} />
-              <DisplayInput value={mobil.telefono} />
+          delivery.map(
+            (mobil, index) => (
+              <div className="delivery__row" key={index}>
+                <DisplayInput
+                  value={mobil.nombre}
+                  setInputValue={(newValue, inputFiel) =>
+                    updateInput(newValue, inputFiel, index)
+                  }
+                  fieldName={"nombre"}
+                />
 
-              <Modal
-                triggerContent={"Valores"}
-                toggleModal={(event) => {
-                  toggleModal(index, event);
-                }}
-                styleButton={true}
-                showModal={showModal === index}
-              >
-                <div className="valueroutes">
-                  <h3>gestion de envios </h3>
-                  <div className="valueroutes__wrapper">
-                    <h4 className="valueroutes__title">Metros </h4>
-                    <div className="valueroutes__container-row">
-                      {Object.entries(mobil.valueDistance).map(
-                        (value, index) => (
-                          <div className="valueroutes__row" key={index}>
-                            <span className="valueroutes__columname">
-                              {value[0]}
-                            </span>
-                            <DisplayInput value={`${value[1]}`} />
-                          </div>
-                        )
-                      )}
+                <DisplayInput
+                  value={mobil.telefono}
+                  setInputValue={(newValue, inputFiel) =>
+                    updateInput(newValue, inputFiel, index)
+                  }
+                  fieldName="telefono"
+                />
+
+                <Modal
+                  triggerContent={"Valores"}
+                  toggleModal={(event) => {
+                    toggleModal(index, event);
+                  }}
+                  styleButton={true}
+                  showModal={showModal === index}
+                >
+                  <div className="valueroutes">
+                    <h3>gestion de envios </h3>
+                    <div className="valueroutes__wrapper">
+                      <h4 className="valueroutes__title">Metros </h4>
+                      <div className="valueroutes__container-row">
+                        {Object.entries(mobil.valueDistance).map(
+                          (value, index2) => (
+                            <div className="valueroutes__row" key={index2}>
+                              <span className="valueroutes__columname">
+                                {value[0]}
+                              </span>
+                              <DisplayInput
+                                value={`${value[1]}`}
+                                setInputValue={(newValue, inputFiel) =>
+                                  updateRouteValue(
+                                    newValue,
+                                    inputFiel,
+                                    index,
+                                    "valueDistance"
+                                  )
+                                }
+                                fieldName={value[0]}
+                              />
+                            </div>
+                          )
+                        )}
+                      </div>
+                    </div>
+                    <div className="valueroutes__wrapper">
+                      <h4 className="valueroutes__title">Metros </h4>
+                      <div className="valueroutes__container-row">
+                        {Object.entries(mobil.valueDelivery).map(
+                          (value, index) => (
+                            <div className="valueroutes__row" key={index}>
+                              <span className="valueroutes__columname">
+                                {value[0]}
+                              </span>
+                              <DisplayInput
+                                value={`${value[1]}`}
+                                setInputValue={(newValue, inputFiel) =>
+                                  updateRouteValue(
+                                    newValue,
+                                    inputFiel,
+                                    index,
+                                    "valueDelivery"
+                                  )
+                                }
+                                fieldName={value[0]}
+                              />
+                            </div>
+                          )
+                        )}
+                      </div>
                     </div>
                   </div>
-                  <div className="valueroutes__wrapper">
-                    <h4 className="valueroutes__title">Metros </h4>
-                    <div className="valueroutes__container-row">
-                      {Object.entries(mobil.valueDelivery).map(
-                        (value, index) => (
-                          <div className="valueroutes__row" key={index}>
-                            <span className="valueroutes__columname">
-                              {value[0]}
-                            </span>
-                            <DisplayInput value={`${value[1]}`} />
-                          </div>
-                        )
-                      )}
-                    </div>
-                  </div>
-                </div>
-              </Modal>
-            </div>
-          ))}
-        <CustomButton size="auto" onClick={addNewDelivery}>añadir</CustomButton>
+                </Modal>
+              </div>
+            )
+            // ))
+          )}
+        <CustomButton size="auto" onClick={addNewDelivery}>
+          añadir
+        </CustomButton>
         <CustomButton onClick={(event) => event.preventDefault()}>
           Guardar
         </CustomButton>
