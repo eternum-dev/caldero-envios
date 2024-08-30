2554e276be044aec9486922df30def6026338e2d
eternum-dev <alejandro.thon.j@gmail.com>
2024-08-30 18:53:08 -0400

    refactor(FormComponent): update field references to match new database schema
    - Updated `locales` field to use `nombreLocal` instead of `local` to align with new database schema.
    - Removed commented-out imports and improved code clarity.
    - Added null check for `locales` to prevent potential errors when mapping over the updated fields.

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,12 +1,10 @@
 import { useContext, useRef } from "react";
 import "./formComponent.css";
 import { InputAutoComplete } from "./InputAutoComplete";
-// import { MapContext } from '../context/MapContext';
 import { useForm } from "../helpers/useForm";
 import { MapContext } from "../context/map/MapContext";
 
 export const FormComponent = () => {
-  //
   const { localCoordinates, local } = useContext(MapContext);
   const inputRef = useRef(null);
   const {
@@ -38,11 +36,12 @@ export const FormComponent = () => {
           value={localCoordinates}
         >
           <option value="seleccionar">Selecciona un local</option>
-          {locales.map((local) => (
-            <option value={local} key={local}>
-              {local}
-            </option>
-          ))}
+          {locales &&
+            locales.map((local) => (
+              <option value={`${local.nombreLocal}`} key={local.nombreLocal}>
+                {local.nombreLocal}
+              </option>
+            ))}
         </select>
       </label>
       <label htmlFor="repartidor" className="formComponent__repartidor">
@@ -56,13 +55,11 @@ export const FormComponent = () => {
           onChange={onSelectRepartidor}
         >
           <option value="seleccionar"> selecciona un repartidor </option>
-          {repartidores.map((deliman, index) => {
-            return (
-              <option key={index} value={`${deliman.nombre}`}>
-                {deliman.nombre}
-              </option>
-            );
-          })}
+          {repartidores.map((deliman, index) => (
+            <option key={index} value={`${deliman.nombre}`}>
+              {deliman.nombre}
+            </option>
+          ))}
         </select>
       </label>
 
