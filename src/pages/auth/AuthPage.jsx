f05668495915c17e8ca625898a3f0eaadfb69ab5
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-02 14:10:01 -0400

    refactor: Refactor AuthPage to use section components
    - Updated AuthPage to import Login and Register components from the home section.
    - Applied Prettier formatting for consistent code style.
    - Simplified conditional rendering for better readability.

--- src/pages/auth/AuthPage.jsx
+++ src/pages/auth/AuthPage.jsx
@@ -1,41 +1,33 @@
-import { useState } from 'react';
-import { Login } from '../../components';
-import { Register } from '../../components/Register';
-import './authpage.css';
+import { useState } from "react";
+
+import "./authpage.css";
+import { Login, Register } from "../../section";
 
 export const AuthPage = () => {
-    const [isRegistered, setIsRegistered] = useState(true);
+  const [isRegistered, setIsRegistered] = useState(true);
 
-    return (
-        <div className='authpage'>
-            <div className='authpage__buttonbox'>
-                <button
-                    onClick={() => setIsRegistered(true)}
-                    className={`authpage__button btn-left ${isRegistered && 'authpage__button--isActive'}`}
-                >
-                    login
-                </button>
-                <button
-                    onClick={() => setIsRegistered(false)}
-                    className={`authpage__button btn-right ${!isRegistered && 'authpage__button--isActive'}`}
-                >
-                    sign-in
-                </button>
-            </div>
+  return (
+    <div className="authpage">
+      <div className="authpage__buttonbox">
+        <button
+          onClick={() => setIsRegistered(true)}
+          className={`authpage__button btn-left ${
+            isRegistered && "authpage__button--isActive"
+          }`}
+        >
+          login
+        </button>
+        <button
+          onClick={() => setIsRegistered(false)}
+          className={`authpage__button btn-right ${
+            !isRegistered && "authpage__button--isActive"
+          }`}
+        >
+          sign-in
+        </button>
+      </div>
 
-
-            {
-                isRegistered ?
-                    (
-                        <Login />
-                    ) :
-                    (
-                        <Register />
-                    )
-            }
-
-
-
-        </div >
-    )
-}
+      {isRegistered ? <Login /> : <Register />}
+    </div>
+  );
+};
