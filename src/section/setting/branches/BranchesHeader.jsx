892a405053f760c50ef847b18c8c040f6e53a6b5
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-02 14:00:15 -0400

    feat: Create BranchesHeader component
    - Moved header section of branches page to a new component BranchesHeader.
    - This component handles the display of the header and its contents.

---
+++ src/section/setting/branches/BranchesHeader.jsx
@@ -0,0 +1,10 @@
+import { Hr } from "../../../components";
+
+export const BranchesHeader = () => {
+  return (
+    <div className="branches__header">
+      <h1>Branches</h1>
+      <Hr />
+    </div>
+  );
+};
