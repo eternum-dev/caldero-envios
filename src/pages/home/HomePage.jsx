3b6e2e49cf63e48e5c14969eb8df8f3a5f651051
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-02 14:11:57 -0400

    refactor: Refactor HomePage to use section components
    - Separated HomePage content into IntroContent and ImageContent components.
    - Updated HomePage to use new section components from the home section.
    - Refactored imports and structure for better readability and organization.

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,25 +1,11 @@
-import { Link } from "react-router-dom";
-import mockPage from "../../assets/mockPage.png";
+import { ImageContent, IntroContent } from "../../section/home";
 import "./homePage.css";
 
 export const HomePage = () => {
   return (
     <div className="homePage">
-      <div className="homePage__wrapper">
-        <h2 className="homePage__title">
-          Optimiza tus Envíos y Mejora la Satisfacción de tus Cliente
-        </h2>
-        <p className="homePage__p">
-          Mejora la experiencia de tus clientes optimizando tus costos y tiempos
-          de envío con precisión
-        </p>
-        <Link to={"/map"}>
-          <button className="homePage__btn">start trial</button>
-        </Link>
-      </div>
-      <div className="homePage__wrapper--image">
-        <img className="homePage__image" src={mockPage} alt="meg" />
-      </div>
+      <IntroContent />
+      <ImageContent />
     </div>
   );
 };
