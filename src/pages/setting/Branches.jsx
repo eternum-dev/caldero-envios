2952694bd8ecfb6214218e73c009aa3ba95d9913
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-02 14:03:55 -0400

    refactor: Refactor Branches component into smaller sections
    - Moved BranchesHeader, BranchesList, and BranchesActions logic into separate components.
    - Updated import paths and component usage in Branches.jsx.
    - Improved component structure for better maintainability and readability.
    - Fix layout.

--- src/pages/setting/Branches.jsx
+++ src/pages/setting/Branches.jsx
@@ -1,17 +1,14 @@
 import { useContext, useEffect, useState } from "react";
-import { CustomButton } from "../../components/CustomButton";
-import { DisplayInput } from "../../components/DisplayInput";
-import { Hr } from "../../components/Hr";
-import "./branches.css";
 import { MapContext } from "../../context/map/MapContext";
-import { Modal } from "../../components/Modal";
-import { updateLocalData } from "../../helpers/updateLocalData";
-import { addBranches } from "../../helpers/branches/addBranches";
 import {
+  updateLocalData,
+  addBranches,
   deleteBranchByIndex,
   updateBranchesField,
   updateCoordBranches,
-} from "../../helpers/branches/branchesStateUtils";
+} from "../../helpers";
+import { BranchesActions, BranchesHeader, BranchesList } from "../../section";
+import "./branches.css";
 
 export const Branches = () => {
   const [branches, setBranches] = useState(null);
@@ -40,83 +37,21 @@ export const Branches = () => {
 
   return (
     <div className="branches">
-      <h1>Branches</h1>
-      <Hr />
-      <form action="">
-        <div className="branches__row branches__row--header">
-          <span>Nombre</span>
-          <span>Numero</span>
-          <span>ubicacion</span>
-        </div>
-        {branches &&
-          branches.map(
-            ({ nombreLocal, numeroLocal, cordenadasLocal }, index) => (
-              <div className="branches__row" key={index}>
-                <DisplayInput
-                  value={nombreLocal}
-                  setInputValue={(newValue, inputFiel) =>
-                    updateBranchesField(newValue, inputFiel, setBranches, index)
-                  }
-                  fieldName="nombreLocal"
-                />
-                <DisplayInput
-                  value={numeroLocal}
-                  setInputValue={(newValue, inputFiel) =>
-                    updateBranchesField(newValue, inputFiel, setBranches, index)
-                  }
-                  fieldName="numeroLocal"
-                />
-
-                <Modal
-                  triggerContent={"coord"}
-                  toggleModal={(event) => toggleModal(index, event)}
-                  showModal={showModal === index}
-                  styleButton={true}
-                >
-                  <div>
-                    <div className="branches__header">
-                      <h3>geolocalizacion </h3>
-                      <CustomButton onClick={() => setShowModal(false)}>
-                        cerrar
-                      </CustomButton>
-                    </div>
-                    <div className="branches__modal">
-                      {Object.entries(cordenadasLocal).map(
-                        (coor, coordIndex) => (
-                          <div key={coordIndex}>
-                            <p>{coor[0]}</p>
-                            <DisplayInput
-                              value={coor[1]}
-                              fieldName={coor[0]}
-                              setInputValue={(newValue, inputField) =>
-                                updateCoordBranches(
-                                  newValue,
-                                  inputField,
-                                  setBranches,
-                                  index,
-                                  "cordenadasLocal"
-                                )
-                              }
-                            />
-                          </div>
-                        )
-                      )}
-                    </div>
-                  </div>
-                </Modal>
-                <CustomButton
-                  onClick={() => deleteBranchByIndex(index, setBranches)}
-                >
-                  x
-                </CustomButton>
-              </div>
-            )
-          )}
-
-        <CustomButton size="fit-content" onClick={addNewBranches}>
-          añadir
-        </CustomButton>
-        <CustomButton onClick={() => updateBranches()}>guardar</CustomButton>
+      <BranchesHeader />
+      <form>
+        <BranchesList
+          branches={branches}
+          setBranches={setBranches}
+          showModal={showModal}
+          toggleModal={toggleModal}
+          updateBranchesField={updateBranchesField}
+          updateCoordBranches={updateCoordBranches}
+          deleteBranchByIndex={deleteBranchByIndex}
+        />
+        <BranchesActions
+          addNewBranches={addNewBranches}
+          updateBranches={updateBranches}
+        />
       </form>
     </div>
   );
