7008e6d860956b6eb834fd248b8fbc9c9057048d
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-03 13:35:21 -0400

    refactor: extract FormHeader component
    - Moved header row markup from Deliveryman component to a new FormHeader component for better readability and separation of concerns.

---
+++ src/section/setting/deliveryman/FormHeader.jsx
@@ -0,0 +1,9 @@
+export const FormHeader = () => {
+  return (
+    <div className="delivery__row delivery__row--header">
+      <span>nombre</span>
+      <span>numero</span>
+      <span>valores</span>
+    </div>
+  );
+};
