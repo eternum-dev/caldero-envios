118c9a2b1771b1b9d9f634fc832f63cda75cdbdd
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-05 19:03:17 -0400

    refactor(helpers): enhance updateProfile function with error handling, JSDoc, and PropTypes
    - Updated `updateProfile` function to return an object with success status and message.
    - Refactored the function to remove direct usage of `setMessage` for improved testability and separation of concerns.
    - Added detailed JSDoc comments for function documentation and usage.
    - Introduced `PropTypes` to validate the input parameters.

--- src/helpers/profile/updateUserprofile.js
+++ src/helpers/profile/updateUserprofile.js
@@ -1,39 +1,56 @@
 import { doc, setDoc } from "firebase/firestore";
 import { auth, db, storage } from "../../firebase/firebase";
 import { getDownloadURL, uploadBytes, ref } from "firebase/storage";
+import PropTypes from "prop-types";
 
+/**
+ * Updates user profile information
+ * @async
+ * @function
+ *
+ * @param {Object} props
+ * @param {String} props.name the user's name
+ * @param {String} props.email the user's email
+ * @param {String} props.profilePictureFile image file saved in a string
+ * @returns {Object} An object that indicates whether the operation was successful and a response message.
+ *
+ * @example
+ *  const { name, email, profilePicture } = profile;
+ *  const response = await updateProfile(name, email, profilePicture);
+ *   setmessage(response.message);
+ */
 
-export const updateProfile = async (
-  name,
-  email,
-  profilePictureFile,
-  setMessage
-) => {
+export const updateProfile = async (name, email, profilePictureFile) => {
   try {
     const user = auth.currentUser;
 
-    // Subir la foto de perfil al Storage
     let profilePictureUrl = profilePictureFile;
     if (profilePictureFile) {
       const storageRef = ref(storage, `fotoPerfil/${user.uid}`);
       await uploadBytes(storageRef, profilePictureFile);
 
-      // devuelve una url del archivo
       if (typeof profilePictureFile !== "string") {
         profilePictureUrl = await getDownloadURL(storageRef);
       }
     }
 
-    // Guardar los datos en Firestore
     await setDoc(doc(db, "usuarios", user.uid), {
       name: name,
       email: email,
       profilePicture: profilePictureUrl,
     });
 
-    setMessage("Perfil actualizado correctamente");
+    return { success: true, message: "Perfil actualizado correctamente" };
   } catch (error) {
-    setMessage("Error al actualizar el perfil:", error);
+    return {
+      success: false,
+      message: `Error al actualizar el perfil: ${error}`,
+    };
   }
 };
 
+updateProfile.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  profilePictureFile: PropTypes.string.isRequired,
+};
