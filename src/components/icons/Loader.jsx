f38ee296667c32d74f2133567cb201daee5b88ba
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-04 23:52:45 -0400

    feat(loader): add new Loader component
    - Created `Loader` component to display a loading animation with pulsing dots.
    - Added accompanying CSS styles in `loader.css` for animation and appearance.

---
+++ src/components/icons/Loader.jsx
@@ -0,0 +1,13 @@
+import "./loader.css";
+
+export const Loader = () => {
+  return (
+    <section className="dots-container loader">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </section>
+  );
+};
