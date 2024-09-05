17f99cd83a0cafe940af874623216433836dcc09
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-05 18:46:18 -0400

    refactor(helpers): improve updateLocalData function with error handling, JSDoc, and PropTypes
    - Updated `updateLocalData` function to return an object with success status and a message.
    - Added detailed JSDoc comments for function documentation and usage examples.
    - Improved error handling for more informative error messages.
    - Included `PropTypes` for input validation and type checking.

--- src/helpers/updateLocalData.js
+++ src/helpers/updateLocalData.js
@@ -1,6 +1,14 @@
 import { doc, setDoc } from "firebase/firestore";
 import { auth, db } from "../firebase/firebase";
+import PropTypes from "prop-types";
 
+/**
+ * actualiza la informacion de los locales almacenados en firebase
+ * @param   {Object}  local - an object with local data.
+ * @param   {Array}   newStateValue - new state to save
+ * @param   {String}  currentItem - name of the document where the data will be saved
+ * @returns {Object}  An object that indicates whether the operation was successful and a response message.
+ */
 export const updateLocalData = async (local, newStateValue, currentItem) => {
   try {
     const user = auth.currentUser;
@@ -9,8 +17,18 @@ export const updateLocalData = async (local, newStateValue, currentItem) => {
       ...local,
       [currentItem]: newStateValue,
     });
-    console.log("acutalizacion completada exitosamente");
+    return { success: true, message: "acutalizacion completada exitosamente" };
   } catch (error) {
-    console.log("erorr: ", error);
+    return { success: false, message: `Error: ${error}` };
   }
+};
+
+updateLocalData.propTypes = {
+  local: PropTypes.shape({
+    nombre: PropTypes.string.isRequired,
+    locales: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+    repartidores: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+  }).isRequired,
+  newStateValue: PropTypes.array.isRequired,
+  currentItem: PropTypes.string.isRequired,
 };
