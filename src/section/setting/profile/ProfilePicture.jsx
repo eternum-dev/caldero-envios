c0dcb84092c4b2d1600c4946368a4c9c995fe343
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-06 10:24:37 -0400

    feat(profile): create ProfilePicture component
    Created ProfilePicture component to handle profile image display and update functionality.
    Includes modal for image selection and input for uploading a new profile picture.

---
+++ src/section/setting/profile/ProfilePicture.jsx
@@ -0,0 +1,100 @@
+import { CustomButton, Modal } from "../../../components";
+import PropTypes from "prop-types";
+
+/**
+ * @component
+ * @example 
+ * const toogleModal = () => { console.log("toogleModal") }
+ * const handleImageChange = () => { console.log("handleImageChange") }
+ * const [picture, setPicture] = useState(""); 
+ * const [profile, setprofile] = useState({});
+ * const [showModal, setShowModal] = useState(false);
+ * 
+ * return (
+ *  <ProfilePicture
+      picture={picture}
+      setPicture={setPicture}
+      profile={profile}
+      setProfile={setProfile}
+      showModal={showModal}
+      setShowModal={setShowModal}
+      toogleModal={toogleModal}
+      handleImageChange={handleImageChange}
+    />
+ * );
+
+ * Description
+ * @param {Object} prop
+ * @param {String} prop.picture
+ * @param {Function} prop.setPicture
+ * @param {Object} prop.profile
+ * @param {Function} prop.setProfile
+ * @param {Number} prop.showModal
+ * @param {Function} prop.setShowModal
+ * @param {Function} prop.toogleModal
+ * @param {Function} prop.handleImageChange
+ * 
+ * @returns {JSX.Element} 
+ * */
+export const ProfilePicture = ({
+  picture,
+  setPicture,
+  profile,
+  showModal,
+  toogleModal,
+  handleImageChange,
+  setProfile,
+  setShowModal,
+}) => {
+  
+  return (
+    <div className="profile__picture">
+      <img
+        className="profile__img"
+        src={`${!picture ? profile.profilePicture : picture}`}
+        alt="otro"
+      />
+      <Modal
+        showModal={showModal === 1}
+        toggleModal={(event) => toogleModal(1, event)}
+        triggerContent={"editar"}
+      >
+        <h4>seleccionar foto</h4>
+        <div className="profile__modal">
+          <input
+            type="file"
+            className="profile__input"
+            accept="image/*"
+            onChange={(event) => handleImageChange(event, setPicture)}
+          />
+          <CustomButton
+            onClick={(event) => {
+              event.preventDefault();
+              setProfile((prev) => {
+                return { ...prev, profilePicture: picture };
+              });
+              setShowModal(false);
+            }}
+          >
+            guardar
+          </CustomButton>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+ProfilePicture.propTypes = {
+  picture: PropTypes.string,
+  setPicture: PropTypes.func.isRequired,
+  profile: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    profilePicture: PropTypes.string.isRequired,
+  }),
+  showModal: PropTypes.oneOfType([PropTypes.any, PropTypes.number.isRequired]),
+  toogleModal: PropTypes.func.isRequired,
+  handleImageChange: PropTypes.func.isRequired,
+  setProfile: PropTypes.func.isRequired,
+  setShowModal: PropTypes.func.isRequired,
+};
