ef987e795b705a4545f90ba1cb95be2fc71ca6f8
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-06 10:24:27 -0400

    feat(profile): create ProfileHeader component
    Created ProfileHeader component to display the title and horizontal line for the profile page.
    This component helps keep the header structure and styling consistent.

---
+++ src/section/setting/profile/ProfileHeader.jsx
@@ -0,0 +1,11 @@
+import { Hr } from "../../../components";
+
+
+export const ProfileHeader = () => {
+  return (
+    <>
+      <h1>Settings Profile</h1>
+      <Hr justify="center" />
+    </>
+  );
+};
