f68ce69bfa7e85a08838b7a5f7c65b5e1803412e
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-07 12:40:41 -0400

    feat(icons): add EditIcon component
    Created a new EditIcon component that provides an SVG representation of an edit icon.

---
+++ src/components/icons/EditIcon.jsx
@@ -0,0 +1,20 @@
+export const EditIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      fill="none"
+      strokeWidth="1.5"
+      color="#000"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#fafafa"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3 21h18M12.222 5.828L15.05 3 20 7.95l-2.828 2.828m-4.95-4.95l-5.607 5.607a1 1 0 00-.293.707v4.536h4.536a1 1 0 00.707-.293l5.607-5.607m-4.95-4.95l4.95 4.95"
+      ></path>
+    </svg>
+  );
+};
