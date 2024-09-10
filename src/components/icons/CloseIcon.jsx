e01f5359fd928baf98e9bc3ddb85cce0cd6c7171
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-09 23:30:34 -0300

    feat(icons): add CloseIcon component
    - Created `CloseIcon` component for use in various parts of the application.
    - Added SVG representation of a close icon with customizable stroke color.

---
+++ src/components/icons/CloseIcon.jsx
@@ -0,0 +1,20 @@
+export const CloseIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      fill="none"
+      strokeWidth="1.5"
+      color="#000"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#ccc"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6.758 17.243L12.001 12m5.243-5.243L12 12m0 0L6.758 6.757M12.001 12l5.243 5.243"
+      ></path>
+    </svg>
+  );
+};
