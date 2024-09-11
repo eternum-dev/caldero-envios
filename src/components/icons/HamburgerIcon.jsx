2a1f795b455863a7778b1d9fbf4d3a3c48c2c2f7
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-11 14:41:37 -0300

    feat(icons): add HamburgerIcon component
    - Created `HamburgerIcon` component for use in the navigation bar.
    - Added SVG representation for a hamburger menu icon with customizable stroke color.

---
+++ src/components/icons/HamburgerIcon.jsx
@@ -0,0 +1,20 @@
+export const HamburgerIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      fill="none"
+      strokeWidth="1.5"
+      color="#fafafa"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#fafafa"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3 5h18M3 12h18M3 19h18"
+      ></path>
+    </svg>
+  );
+};
