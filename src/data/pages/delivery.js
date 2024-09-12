ae878d42dedb3578bedba1b77b249dd754f230a7
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-12 18:33:58 -0300

    refactor(data): create delivery.js for storing deliveryman content data

---
+++ src/data/pages/delivery.js
@@ -0,0 +1,14 @@
+export const delivery = {
+  title: "Repartidores",
+};
+
+export const  rowHeader = {
+    name: "Nombre",
+    phoneNumber: "Numero",
+    values: "Valores",
+}
+
+export const manageActions = {
+    addButton: "Añadir", 
+    saveButton: "Guardar"
+}
