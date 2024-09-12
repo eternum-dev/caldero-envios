a394ed28451af0fd7fe99bbe2babda2fd87d734a
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-12 18:33:12 -0300

    refactor(data): create home.js for storing home page content data

---
+++ src/data/pages/home.js
@@ -0,0 +1,14 @@
+export const introContent = {
+    title: "Optimiza tus Envíos y Mejora la Satisfacción de tus Cliente",
+    paragraph:
+      "Mejora la experiencia de tus clientes optimizando tus costos y tiempos de envío con precisión.",
+    link: {
+      start: "Start Trial",
+    },
+  };
+  
+  export const imageContent = {
+    url: "/src/assets/mockPage.png",
+    alt: "map page",
+  };
+  
