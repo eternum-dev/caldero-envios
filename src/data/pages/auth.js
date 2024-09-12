fdbcc2ae2e1dd811e347c4ab545462c64b9f9586
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-12 18:33:27 -0300

    refactor(data): create auth.js for storing authentication content data

---
+++ src/data/pages/auth.js
@@ -0,0 +1,20 @@
+export const auth = {
+  buttons: {
+    login: "Login",
+    signIn: "sign-in",
+  },
+};
+
+export const login = {
+  title: "Hola de nuevo!",
+  divider: { start: "Inicia con", end: "O puedes usar" },
+  image: "",
+  button: "Enviar",
+};
+
+export const signIn = {
+  title: "Construyamos un mapa juntos!",
+  divider: { start: "Registrate con", end: "o tambien puedes hacerlo por" },
+  image: "",
+  button: "Registrarme",
+};
