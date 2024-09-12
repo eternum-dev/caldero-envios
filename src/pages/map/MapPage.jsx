8ddb564f2d490b53c1ff0feb601b1c4338a8e9a9
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-12 18:47:38 -0300

    style(code): reformat and tidy code for MapPage component

--- src/pages/map/MapPage.jsx
+++ src/pages/map/MapPage.jsx
@@ -1,43 +1,37 @@
-import { Map, Marker } from '@vis.gl/react-google-maps';
-import { useContext } from 'react';
-import { MapContext } from '../../context/map/MapContext';
-import { Direction, FormComponent, MapRoute, styleMapDark } from '../../components';
-import './mapPage.css';
+import { Map, Marker } from "@vis.gl/react-google-maps";
+import { useContext } from "react";
+import { MapContext } from "../../context/map/MapContext";
+import {
+  Direction,
+  FormComponent,
+  MapRoute,
+  styleMapDark,
+} from "../../components";
+import "./mapPage.css";
 
 export const MapPage = () => {
+  const { localCoordinates, renderRoute } = useContext(MapContext);
 
-    const { localCoordinates, renderRoute } = useContext(MapContext);
-
-
-
-    return (
-
-        <div className='map'>
-            <Map zoom={15}
-                center={localCoordinates}
-                gestureHandling={'greedy'}
-                disableDefaultUI={true}
-                styles={styleMapDark}
-            >
-                {
-                    renderRoute ?
-                        <MapRoute>
-                            <Direction localCordinates={localCoordinates} />
-                        </MapRoute>
-                        :
-                        <>
-                            <FormComponent />
-                            <Marker
-                                position={localCoordinates}
-                            />
-                        </>
-                }
-            </Map>
-
-        </div >
-    )
-}
-
-
-
-
+  return (
+    <div className="map">
+      <Map
+        zoom={15}
+        center={localCoordinates}
+        gestureHandling={"greedy"}
+        disableDefaultUI={true}
+        styles={styleMapDark}
+      >
+        {renderRoute ? (
+          <MapRoute>
+            <Direction localCordinates={localCoordinates} />
+          </MapRoute>
+        ) : (
+          <>
+            <FormComponent />
+            <Marker position={localCoordinates} />
+          </>
+        )}
+      </Map>
+    </div>
+  );
+};
