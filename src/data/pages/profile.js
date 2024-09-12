286ed679afa4fe7103b84869b8b65219123cd009
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-12 18:34:49 -0300

    refactor(data): create profile.js for storing profile settings content data

---
+++ src/data/pages/profile.js
@@ -0,0 +1,12 @@
+export const header = {
+  title: "Settings Profile",
+};
+
+export const picture = {
+  title: "Seleccionar foto",
+  button: "Guardar",
+};
+
+export const uploaderImage = {
+  button: "Subir Foto",
+};
