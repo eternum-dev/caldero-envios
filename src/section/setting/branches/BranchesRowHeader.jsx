62d696d3b9019cea3dde7554b63e2b1b887273ad
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-12 18:48:24 -0300

    feat(components): create BranchesRowHeader component with initial styles

---
+++ src/section/setting/branches/BranchesRowHeader.jsx
@@ -0,0 +1,9 @@
+export const BranchesRowHeader = () => {
+  return (
+    <div className="branches__row branches__row--header">
+      <span>Nombre</span>
+      <span>Numero</span>
+      <span>Ubicacion</span>
+    </div>
+  );
+};
