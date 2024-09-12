cf7af76d040324eea57fc2c5ca0229bbb4fdb803
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-12 18:35:22 -0300

    refactor(data): create header.js for storing header and navigation content data

---
+++ src/data/layout/header.js
@@ -0,0 +1,19 @@
+export const headerData = {
+  title: "Caldero Envios",
+  navBar: {
+    profile: {
+      title: "Configuracion de usuario",
+      links: {
+        profile: "Perfil",
+        password: "password",
+      },
+    },
+    businesses: {
+      title: "Panel de Control",
+      links: {
+        delivery: "Repartidor",
+        branches: "locales",
+      },
+    },
+  },
+};
