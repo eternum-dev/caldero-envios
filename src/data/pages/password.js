6ecc79a7b52f7c9c2803267320cca94d1dcd99fd
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-12 18:34:36 -0300

    refactor(data): create password.js for storing password management content data

---
+++ src/data/pages/password.js
@@ -0,0 +1,12 @@
+export const header = {
+  title: "Actualizar contraseña",
+};
+
+export const form = {
+  button: "Guardar",
+  input: {
+    currentPassword: "Contraseña actual",
+    newPassword: "Nueva contraseña",
+    repeatPassword: "Repite la Contraseña",
+  },
+};
