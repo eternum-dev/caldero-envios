b65b45d63ab3768e0a08e3fe4b72833d07bf04e9
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-12 18:34:17 -0300

    refactor(data): create map.js for storing map and route-related content data

---
+++ src/data/pages/map.js
@@ -0,0 +1,26 @@
+export const mapRoute = {
+  loading: "loading...",
+  paragraph: {
+    direction: "La direccion de envio es:",
+    time: "Tiempo aprox de envio:",
+    value: "Valor del envio:",
+    distance: "Distancia: ",
+  },
+};
+
+export const buttonReset = "Restaurar Mapa";
+
+export const formComponent = {
+  direction: {
+    label: "Direccion",
+  },
+  deliveryman: {
+    label: "Repartidor",
+    defaultOption: "Selecciona un repartidor",
+  },
+  branches: {
+    label: "Sucursal",
+    defaultOption: "Selecciona la sucursal inicial",
+  },
+  buttonSubmit: "Calcular",
+};
