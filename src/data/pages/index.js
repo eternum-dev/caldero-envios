475a76af8c13333f40279e0046b3df46d5c9214b
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-12 18:35:12 -0300

    refactor(data): create index.js for exporting all page-related data files

---
+++ src/data/pages/index.js
@@ -0,0 +1,7 @@
+export * from './auth';
+export * from './branches';
+export * from './delivery';
+export * from './home';
+export * from './map';
+export * from './password';
+export * from './profile';
