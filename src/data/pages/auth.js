76da328ddf31670d9cb3a05f4f93f9650851377e
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-13 12:05:35 -0300

    refactor(data): update login and signIn objects to include input fields
    Updated the data objects 'login' and 'signInData' to include input field details for email, password, and other user credentials. Changed the button naming for consistency.

--- src/data/pages/auth.js
+++ src/data/pages/auth.js
@@ -9,12 +9,22 @@ export const login = {
   title: "Hola de nuevo!",
   divider: { start: "Inicia con", end: "O puedes usar" },
   image: "",
-  button: "Enviar",
+  saveButton: "Enviar",
+  input: {
+    email: { name: "Email", type: "email" },
+    password: { name: "Password", type: "password" },
+  },
 };
 
-export const signIn = {
+export const signInData = {
   title: "Construyamos un mapa juntos!",
   divider: { start: "Registrate con", end: "o tambien puedes hacerlo por" },
   image: "",
-  button: "Registrarme",
+  saveButton: "Registrarme",
+  input: {
+    name: { name: "name", type: "text" },
+    email: { name: "Email", type: "email" },
+    password: { name: "Password", type: "password" },
+    repeatPassword: { name: "Repeat Password", type: "password" },
+  },
 };
