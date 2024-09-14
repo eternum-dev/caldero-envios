27fbc283c49f15e599103b4e861e7b6031a1d0af
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-14 15:23:58 -0300

    refactor(BranchesHeader): extract title from external data file
    Moved the title in `BranchesHeader` to an external data file.

--- src/section/setting/branches/BranchesHeader.jsx
+++ src/section/setting/branches/BranchesHeader.jsx
@@ -1,9 +1,11 @@
 import { Hr } from "../../../components";
+import { branchesHeader } from "../../../data";
 
 export const BranchesHeader = () => {
+  const { title } = branchesHeader;
   return (
     <div className="branches__header">
-      <h1>Branches</h1>
+      <h1>{title}</h1>
       <Hr />
     </div>
   );
