6a1521e5ec07ae711ac139cf8034bc619eec4d51
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-14 15:24:31 -0300

    refactor(BranchesRowHeader): extract column headers from external data
    Modified `BranchesRowHeader` to use external data for column headers: "Nombre", "Numero", and "Ubicación" to ensure flexibility.

--- src/section/setting/branches/BranchesRowHeader.jsx
+++ src/section/setting/branches/BranchesRowHeader.jsx
@@ -1,9 +1,13 @@
+import { branchesRowHeader } from "../../../data";
+
 export const BranchesRowHeader = () => {
+  const { location, name, phoneNumber } = branchesRowHeader;
+
   return (
     <div className="branches__row branches__row--header">
-      <span>Nombre</span>
-      <span>Numero</span>
-      <span>Ubicacion</span>
+      <span>{name}</span>
+      <span>{phoneNumber}</span>
+      <span>{location}</span>
     </div>
   );
 };
