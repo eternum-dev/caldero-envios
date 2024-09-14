9525e63e54dfe5809087d522097245b5a0deedf0
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-14 13:55:14 -0300

    refactor(direction): extract hardcoded button text to data file
    Moved reset button text to the `data` directory for better separation of concerns.

--- src/components/Direction.jsx
+++ src/components/Direction.jsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 import { useMyDirectionService } from "../helpers";
+import { buttonReset } from "../data";
 
 export const Direction = ({ localCordinates }) => {
   const { resetMap } = useMyDirectionService({ localCordinates });
 
   return (
     <div className="directions">
-      <button onClick={resetMap}> reset</button>
+      <button onClick={resetMap}>{buttonReset}</button>
     </div>
   );
 };
