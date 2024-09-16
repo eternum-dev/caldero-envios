8f218fafbdc35c3e7ce4e31660e8e230f995bd9c
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-16 18:27:05 -0300

    feat(profile): refactor profile settings and image uploader - Renamed `header` to `profileHeader`. - Updated `picture` to `profilePicture` and added structure to `profileUploaderImage`.

--- src/data/pages/profile.js
+++ src/data/pages/profile.js
@@ -1,12 +1,18 @@
-export const header = {
+export const dataProfile = {
+  docItem: "profilePicture",
+  button: "Guardar"
+}
+
+export const profileHeader = {
   title: "Settings Profile",
 };
 
-export const picture = {
+export const profilePicture = {
   title: "Seleccionar foto",
   button: "Guardar",
+  
 };
 
-export const uploaderImage = {
+export const profileUploaderImage = {
   button: "Subir Foto",
 };
