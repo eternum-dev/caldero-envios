2819381d35af9e5bdbce7ae4c597e594cb46d53f
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-16 18:15:33 -0300

    refactor(profile-header): move title to data file and adjust hr alignment

--- src/section/setting/profile/ProfileHeader.jsx
+++ src/section/setting/profile/ProfileHeader.jsx
@@ -1,10 +1,11 @@
 import { Hr } from "../../../components";
-
+import { profileHeader } from "../../../data";
 
 export const ProfileHeader = () => {
+  const { title } = profileHeader;
   return (
     <>
-      <h1>Settings Profile</h1>
+      <h1>{title}</h1>
       <Hr justify="center" />
     </>
   );
