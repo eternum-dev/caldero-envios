9821fe7a52261ecf4325d01dd6ff4edc03fae84b
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-26 18:48:21 -0300

    feat(firebase): add Firebase Functions initialization
    - Added `getFunctions` to Firebase initialization to support cloud functions.
    - Updated environment variable handling for API keys and Firebase configuration.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getFunctions } from "firebase/functions";
 
 //*  Firebase configuration
 
@@ -28,5 +29,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const functions = getFunctions(app);
 
-export { app, auth, db, storage };
+export { app, auth, db, storage, functions };
