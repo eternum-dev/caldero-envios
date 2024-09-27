ae1787482a5574c2a4a0dfc8aed9135fdbaf99e6
eternum-dev <alejandro.thon.j@gmail.com>
2024-09-27 19:07:56 -0300

    feat(helpers): create whatsappNotifier helper
    -Created whatsappNotifier.js to handle the logic for sending WhatsApp messages through Twilio. This helper centralizes the message-sending logic, improving code reuse and maintainability

---
+++ src/helpers/whatsappNotifier.js
@@ -0,0 +1,26 @@
+export const whatsappNotifier = async () => {
+  try {
+    const response = await fetch(
+      "https://us-central1-caldero-envios.cloudfunctions.net/sendWhatsAppMessage",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          to: "+56959141570",
+          bodyMessage: "Hola, aquí tienes la información del envío.",
+        }),
+      }
+    );
+    const result = await response.json();
+
+    if (result.success) {
+      alert("Mensaje enviado con éxito!");
+    } else {
+      alert("Error al enviar el mensaje");
+    }
+  } catch (error) {
+    console.error("error: ", error);
+  }
+};
