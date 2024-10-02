9e2e3b158c1cdee7fd93cea2caca158f79f66daa
eternum-dev <alejandro.thon.j@gmail.com>
2024-10-01 23:12:23 -0300

    refactor(MapProvider): include new state for nameLocal and refactor useEffect logic

--- src/context/map/MapProvider.jsx
+++ src/context/map/MapProvider.jsx
@@ -1,60 +1,58 @@
-import { useContext, useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import { MapContext } from './MapContext';
-import { getLocal } from '../../helpers/getLocal';
-import { AuthContext } from '../auth/AuthContext';
+import { useContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { MapContext } from "./MapContext";
+import { getLocal } from "../../helpers/getLocal";
+import { AuthContext } from "../auth/AuthContext";
 
 const initialCoords = { lat: -39.83823263400049, lng: -73.21039410495716 };
 
 export const MapProvider = ({ children }) => {
-    const [localCoordinates, setLocalCoordinates] = useState(initialCoords);
-    const [repartidor, setRepartidor] = useState('');
-    const [destination, setDestination] = useState('');
-    const [renderRoute, setRenderRoute] = useState(false);
-    const [dataRoute, setDataRoute] = useState([]);
-    const [local, setLocal] = useState(null);
+  const [localCoordinates, setLocalCoordinates] = useState(initialCoords);
+  const [nameLocal, setNameLocal] = useState("");
+  const [repartidor, setRepartidor] = useState("");
+  const [destination, setDestination] = useState("");
+  const [renderRoute, setRenderRoute] = useState(false);
+  const [dataRoute, setDataRoute] = useState([]);
+  const [local, setLocal] = useState(null);
 
-    const { user } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
-    useEffect(() => {
-        if(!user) return;
-        const fetchLocal = async () => {
-            try {
-                const localData = await getLocal();
+  useEffect(() => {
+    if (!user) return;
+    const fetchLocal = async () => {
+      try {
+        const localData = await getLocal();
 
-                setLocal(localData);
-            } catch (error) {
-                throw new Error(error);
-            }
-        };
-        fetchLocal();
-    }, [user]);
-
-
-    const contextValue = {
-        localCoordinates,
-        setLocalCoordinates,
-        repartidor,
-        setRepartidor,
-        destination,
-        setDestination,
-        renderRoute,
-        setRenderRoute,
-        dataRoute,
-        setDataRoute,
-        local,
-        setLocal
+        setLocal(localData);
+      } catch (error) {
+        throw new Error(error);
+      }
     };
+    fetchLocal();
+  }, [user]);
 
+  const contextValue = {
+    localCoordinates,
+    setLocalCoordinates,
+    repartidor,
+    nameLocal,
+    setNameLocal,
+    setRepartidor,
+    destination,
+    setDestination,
+    renderRoute,
+    setRenderRoute,
+    dataRoute,
+    setDataRoute,
+    local,
+    setLocal,
+  };
 
-
-    return (
-        <MapContext.Provider value={contextValue}>
-            {children}
-        </MapContext.Provider>
-    )
-}
+  return (
+    <MapContext.Provider value={contextValue}>{children}</MapContext.Provider>
+  );
+};
 
 MapProvider.propTypes = {
-    children: PropTypes.any
-}
+  children: PropTypes.any,
+};
