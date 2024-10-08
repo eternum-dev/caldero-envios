ba10d42a249ca57d08a19137cae27529d5ba4b25
eternum-dev <alejandro.thon.j@gmail.com>
2024-10-07 22:56:55 -0300

    refactor(functions): extract firebase admin initialization and add buildLocalTemplate functionality

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,11 @@
+const admin = require("firebase-admin");
+
+const { buildLocalTemplate } = require("./src/buildLocalTemplate");
 const { copyUserToBusiness } = require("./src/copyUserToBusiness");
 const { sendWhatsAppMessage } = require("./src/sendWhatsAppMessage");
 
+admin.initializeApp();
+
 exports.sendWhatsAppMessage = sendWhatsAppMessage;
 exports.copyUserToBusiness = copyUserToBusiness;
+exports.buildLocalTemplate = buildLocalTemplate;
