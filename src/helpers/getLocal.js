42e998aa0c53aec1c565fd53e176e9153edcb07d
eternum-dev <alejandro.thon.j@gmail.com>
2024-10-16 19:14:29 -0300

    docs(helpers): uptdate JSDoc

--- src/helpers/getLocal.js
+++ src/helpers/getLocal.js
@@ -2,20 +2,17 @@ import { doc, getDoc } from "firebase/firestore";
 import { auth, db } from "../firebase/firebase";
 
 export const getLocal = async () => {
+  const user = auth.currentUser;
+  
+  if (user) {
+    const docRef = doc(db, "local", user.email);
+    const docSnap = await getDoc(docRef);
 
-    const user = auth.currentUser;
-
- 
-    if (user) {
-        const docRef = doc(db, "local", user.email);
-        const docSnap = await getDoc(docRef);
-
-        if (docSnap.exists()) {
-            console.log(docSnap.data());
-            return docSnap.data();
-        } else {
-            return null;
-        }
+    if (docSnap.exists()) {
+      console.log(docSnap.data());
+      return docSnap.data();
+    } else {
+      return null;
     }
-}
-
+  }
+};
