b8a19e051cf8e8d2deb6eea899ce12ecff819227
eternum-dev <alejandro.thon.j@gmail.com>
2024-10-17 18:03:57 -0300

    style(context): reformat code in AuthProvider for consistency
    - Reformatted code in `AuthProvider` to ensure consistency in quotes, spacing, and structure.
    - No functional changes, only improved code homogeneity.

--- src/context/auth/AuthProvider.jsx
+++ src/context/auth/AuthProvider.jsx
@@ -1,50 +1,44 @@
-import { useEffect, useState } from 'react';
-import { onAuthStateChanged } from 'firebase/auth';
-
-import PropTypes from 'prop-types';
-
-import { AuthContext } from './AuthContext';
-import { auth } from '../../firebase/firebase';
-import { createUser, signIn, signInWithGoogle, signOut } from '../../firebase/auth';
-
-
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import PropTypes from "prop-types";
+import { AuthContext } from "./AuthContext";
+import { auth } from "../../firebase/firebase";
+import {
+  createUser,
+  signIn,
+  signInWithGoogle,
+  signOut,
+} from "../../firebase/auth";
 
 export const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(false);
 
-    const [loading, setLoading] = useState(true);
-    const [user, setUser] = useState(false);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      }
+      setLoading(false);
+    });
 
+    return unsubscribe;
+  }, []);
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            if (currentUser) {
-                setUser(currentUser); 
-            }
+  const value = {
+    loading,
+    user,
+    setUser,
 
-            setLoading(false);
-        });
+    createUser,
+    signIn,
+    signInWithGoogle,
+    signOut,
+  };
 
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
 
-        return unsubscribe;
-    }, []);
-
-    const value = {
-        loading,
-        user,
-        setUser,
-
-        createUser,
-        signIn,
-        signInWithGoogle,
-        signOut
-    }
-
-    return (
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
 AuthProvider.propTypes = {
-    children: PropTypes.any
-}
+  children: PropTypes.any,
+};
