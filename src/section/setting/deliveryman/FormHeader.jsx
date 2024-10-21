73581b730f133c2f3188efacadb2880deda099ce
eternum-dev <alejandro.thon.j@gmail.com>
2024-10-21 13:11:28 -0300

    feat(FormHeader): include CloseIcon component for improved UI - Added `CloseIcon` component for better user interaction. - Enhanced the UI by providing a close button.

--- src/section/setting/deliveryman/FormHeader.jsx
+++ src/section/setting/deliveryman/FormHeader.jsx
@@ -1,13 +1,17 @@
+import { CloseIcon } from "../../../components";
 import { deliveryRowHeader } from "../../../data";
 
 export const FormHeader = () => {
   const { name, phoneNumber, values } = deliveryRowHeader;
-  
+
   return (
     <div className="delivery__row delivery__row--header">
       <span>{name}</span>
       <span>{phoneNumber}</span>
       <span>{values}</span>
+      <span>
+        <CloseIcon height="24" width="24"/>
+      </span>
     </div>
   );
 };
