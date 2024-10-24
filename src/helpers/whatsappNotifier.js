1016633a2f35a39fc8cbd31fb1962db975c48ef4
eternum-dev <alejandro.thon.j@gmail.com>
2024-10-24 18:34:28 -0300

    refactor: add deliveryPhoneNumber parameter to whatsappNotifier function and implement error handling

--- src/helpers/whatsappNotifier.js
+++ src/helpers/whatsappNotifier.js
@@ -1,14 +1,24 @@
-export const whatsappNotifier = async () => {
+/**
+ *
+ * @param {string} deliveryPhoneNumber
+ */
+
+export const whatsappNotifier = async (deliveryPhoneNumber) => {
   const URL_WHATSAPP_NOTIFIER = import.meta.env.VITE_URL_WHATSAPP_NOTIFIER;
 
   try {
+    if (!deliveryPhoneNumber) {
+      console.error("no se ha seleccionado un repartidor");
+      return;
+    }
+
     const response = await fetch(URL_WHATSAPP_NOTIFIER, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        to: "+56959141570",
+        to: deliveryPhoneNumber,
         bodyMessage: "Hola, aquí tienes la información del envío.",
       }),
     });
