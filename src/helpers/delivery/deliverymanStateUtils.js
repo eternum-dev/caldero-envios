75ed9ad7ec634b918cb255bde85dcda7301fbdd1
eternum-dev <alejandro.thon.j@gmail.com>
2024-10-31 18:19:24 -0300

    feat(helpers): enhance delivery metrics and field update functions
    Refactored updateDeliveryMetrics to handle nested metric updates in delivery items.
    Added deleteMetricsByIndex and addMetrics for dynamic management of metric arrays.
    Updated deletedeliveryByIndex to improve branch filtering by index.

--- src/helpers/delivery/deliverymanStateUtils.js
+++ src/helpers/delivery/deliverymanStateUtils.js
@@ -6,21 +6,21 @@ export const updateDeliveryMetrics = (
   currentItem
 ) => {
   setDelivery((prev) =>
-    prev.map((item, i) =>
-      i === index
-        ? {
-            ...item,
-            [currentItem]: {
-              ...item[currentItem],
-              [fieldName]: newValue * 1,
-            },
-          }
-        : item
-    )
+    prev.map((item) => ({
+      ...item,
+      [currentItem]: item[currentItem].map((field, i) =>
+        index === i ? { ...field, [fieldName]: newValue } : field
+      ),
+    }))
   );
 };
 
-export const updateDeliveryField = (setDelivery, newValue, fieldName, index) => {
+export const updateDeliveryField = (
+  setDelivery,
+  newValue,
+  fieldName,
+  index
+) => {
   setDelivery((prev) =>
     prev.map((item, i) =>
       i === index ? { ...item, [fieldName]: newValue } : item
@@ -30,4 +30,42 @@ export const updateDeliveryField = (setDelivery, newValue, fieldName, index) => 
 
 export const deletedeliveryByIndex = (indexToDelete, setBranches) => {
   setBranches((prev) => prev.filter((_, index) => index !== indexToDelete));
+};
+
+export const deleteMetricsByIndex = (
+  setDelivery,
+  currentItem,
+  index,
+  indexMetrics
+) => {
+  setDelivery((prev) => {
+    const newState = prev.map((item, indexDelivery) =>
+      indexDelivery === index
+        ? {
+            ...item,
+            [currentItem]: item[currentItem].filter((_, i) => {
+              return i !== indexMetrics;
+            }),
+          }
+        : item
+    );
+
+    return newState;
+  });
+};
+
+export const addMetrics = (setDelivery, currentItem, index) => {
+  setDelivery((prev) =>
+    prev.map((item, indexDelivery) =>
+      indexDelivery === index
+        ? {
+            ...item,
+            [currentItem]: [
+              ...item[currentItem],
+              { name: "nuevo item", totalCost: 10, distanceKilometers: 10 },
+            ],
+          }
+        : item
+    )
+  );
 };
