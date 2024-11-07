26a824d9c53ffc232801e0eb08d0ea863b3a5f5c
eternum-dev <alejandro.thon.j@gmail.com>
2024-11-07 19:02:57 -0300

    feat(DeliveryMetrics): add state and dynamic metric update functionality
    - Introduced `columName` state to handle dynamic column name changes.
    - Added UUID for key management in mapped elements.
    - Integrated new prop functions `addMetrics` and `deleteMetricsByIndex` for managing delivery metrics.

--- src/section/setting/deliveryman/Metrics.jsx
+++ src/section/setting/deliveryman/Metrics.jsx
@@ -1,28 +1,71 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
-import { DisplayInput } from "../../../components";
+import { CloseIcon, CustomButton, DisplayInput } from "../../../components";
 
-export const DeliveryMetrics = ({ title, data, updateMetrics }) => (
-  <div className="valueroutes__wrapper">
-    <h4 className="valueroutes__title">{title}</h4>
-    <div className="valueroutes__container-row">
-      {Object.entries(data).map((value, index) => (
-        <div className="valueroutes__row" key={index}>
-          <span className="valueroutes__columname">{value[0]}</span>
-          <DisplayInput
-            value={`${value[1]}`}
-            setInputValue={(newValue, inputFiel) =>
-              updateMetrics(newValue, inputFiel)
-            }
-            fieldName={value[0]}
-          />
-        </div>
-      ))}
+import { v4 as uuidv4 } from "uuid";
+
+export const DeliveryMetrics = ({
+  title,
+  data,
+  updateMetrics,
+  addMetrics,
+  deleteMetricsByIndex,
+}) => {
+  const [columName, setColumName] = useState("");
+
+  return (
+    <div className="valueroutes__wrapper">
+      <h4 className="valueroutes__title">{title}</h4>
+      <div className="valueroutes__container-row">
+        {data
+          .sort((a, b) => a.distanceKilometers - b.distanceKilometers)
+          ?.map(({ name, distanceKilometers, totalCost }, index) => (
+            <div className="valueroutes__row" key={uuidv4()}>
+              <input
+                type="text"
+                className="valueroutes__columname"
+                value={columName || name}
+                onChange={(event) => setColumName(event.target.value)}
+              />
+              <DisplayInput
+                value={distanceKilometers}
+                setInputValue={(newValue) =>
+                  updateMetrics(
+                    newValue * 1,
+                    Object.keys({ distanceKilometers }),
+                    index
+                  )
+                }
+                fieldName={name}
+              />
+              <DisplayInput
+                value={totalCost * 1}
+                setInputValue={(newValue) =>
+                  updateMetrics(newValue, Object.keys({ totalCost }), index)
+                }
+                fieldName={name}
+              />
+              <CustomButton onClick={() => deleteMetricsByIndex(index)}>
+                <CloseIcon />
+              </CustomButton>
+            </div>
+          ))}
+        <CustomButton
+          onClick={() => {
+            addMetrics();
+          }}
+        >
+          eliminar
+        </CustomButton>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 DeliveryMetrics.propTypes = {
   title: PropTypes.string.isRequired,
-  data: PropTypes.object.isRequired,
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
   updateMetrics: PropTypes.func.isRequired,
+  addMetrics: PropTypes.func.isRequired,
+  deleteMetricsByIndex: PropTypes.func.isRequired,
 };
