1c21aca049ace614eb53ead6946a25d2534871bf
eternum-dev <alejandro.thon.j@gmail.com>
2024-11-13 18:42:12 -0300

    feat(data): update home page text content in home.js for readability
    - Correct grammar in introContent title and paragraph for clarity.
    - Translate link text to Spanish for language consistency.
    - Update image alt text for improved accessibility.

--- src/data/pages/home.js
+++ src/data/pages/home.js
@@ -1,14 +1,13 @@
 export const introContent = {
-    title: "Optimiza tus Envíos y Mejora la Satisfacción de tus Cliente",
-    paragraph:
-      "Mejora la experiencia de tus clientes optimizando tus costos y tiempos de envío con precisión.",
-    link: {
-      start: "Start Trial",
-    },
-  };
-  
-  export const imageContent = {
-    url: "/src/assets/mockPage.png",
-    alt: "map page",
-  };
-  
+  title: "Optimiza tus Envíos y Mejora la Satisfacción de tus Clientes",
+  paragraph:
+    "Optimiza los costos y tiempos de envío para mejorar la experiencia de tus clientes.",
+  link: {
+    start: "Iniciar Prueba",
+  },
+};
+
+export const imageContent = {
+  url: "/src/assets/mockPage.png",
+  alt: "página de mapa",
+};
