3d192f2df1d8b5e20c82b15a7fbff8afa41ef27d
eternum-dev <alejandro.thon.j@gmail.com>
2024-11-19 23:07:35 -0300

    fix(calculate-route): fallback to the first metric if no match is found
    - Added logic to assign the first metric as the default when no other conditions are met in `calculateValueRute`.
    - Ensures accurate and cost-effective route calculation.

--- src/helpers/calculateValueRute.js
+++ src/helpers/calculateValueRute.js
@@ -2,8 +2,10 @@ export const calculateValueRute = (distance, repartidor) => {
   let valueRute = 0;
   const { metrics } = repartidor;
 
-  const findMetrics = metrics.find(
-    (metric) => metric.distanceKilometers <= distance
+  const findMetrics = metrics.find((metric) =>
+    metric.distanceKilometers <= distance
+      ? metric
+      : metrics[0]
   );
   valueRute = findMetrics.totalCost;
 
