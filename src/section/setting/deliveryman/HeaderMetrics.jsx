8b5fbd67d0005fa5a4f7818ffac87fa2318f105e
eternum-dev <alejandro.thon.j@gmail.com>
2024-11-27 09:48:54 -0300

    feat(header-metrics): add header component for metrics
    - Created `HeaderMetrics` to display metric titles with a close icon.

---
+++ src/section/setting/deliveryman/HeaderMetrics.jsx
@@ -0,0 +1,13 @@
+import { CloseIcon } from "../../../components";
+
+export const HeaderMetrics = () => {
+  return (
+    <div className="headerMetrics">
+      <span>{"metros  entre"}</span>
+      <span>{"valor"}</span>
+      <span>
+        <CloseIcon height="24" width="24" />
+      </span>
+    </div>
+  );
+};
