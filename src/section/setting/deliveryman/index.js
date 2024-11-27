280f72b73b0d24b1a902f10e6e839e3224bf3fbb
eternum-dev <alejandro.thon.j@gmail.com>
2024-11-27 09:50:03 -0300

    chore(exports): update barrel file with new component

--- src/section/setting/deliveryman/index.js
+++ src/section/setting/deliveryman/index.js
@@ -1,5 +1,6 @@
 export * from './DeliveryValuesModal';
 export * from './FormHeader';
+export * from './HeaderMetrics';
 export * from './index';
 export * from './Metrics';
 export * from './RowItem';
