6697631e7d62e42f0a7e2b001e67e8449f9002be
eternum-dev <alejandro.thon.j@gmail.com>
2024-12-02 16:53:40 -0300

    feat(utils): add stringCapitalization helper function
    - Added `stringCapitalization` helper function in `utils/stringUtils.js`.
    - Converts the first letter of a string to uppercase and normalizes the rest to lowercase.
    - Includes JSDoc for better documentation and examples.

--- src/helpers/utils/stringUtils.js
+++ src/helpers/utils/stringUtils.js
@@ -16,3 +16,30 @@ export const normalizeString = (text) => {
     .replace(/([aeio])\u0301|(u)[\u0301\u0308]/gi, "$1$2")
     .normalize();
 };
+
+/**
+ * Capitalizes a string by converting the first letter to uppercase
+ * keeping the remaining letters lowercase and standardizing its format.
+ *
+ * @param {string} text - The string to Capitalize.
+ * @returns {string} The Capitalize string  or an empty string if input is invalid.
+ *
+ * @example
+ * stringCapitalization("title");
+ * // Returns "Title"
+ * @example
+ * stringCapitalization("");
+ * // Returns ""
+ *
+ * @example
+ * stringCapitalization(123);
+ * // Returns ""
+ */
+
+export const stringCapitalization = (text) => {
+  if (typeof text !== "string" || text.length === 0) {
+    return "";
+  }
+
+  return text[0].toUpperCase() + text.slice(1).toLowerCase();
+};
