a6f1aff94682051ef65b413e007c02dcb3c07df3
eternum-dev <alejandro.thon.j@gmail.com>
2024-12-12 15:26:03 -0300

    refactor(router): enhance AppRouter with initial path and loading state
    Updated `AppRouter` to include:
    - A redirect from the initial path (`/`) based on user authentication status.
    - A loading state to handle authentication context readiness.

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -14,31 +14,43 @@ import { AuthContext } from "../context/auth/AuthContext";
 import { routePaths } from "../data";
 
 export const AppRouter = () => {
-  const { user } = useContext(AuthContext);
-  const { otherRoute, privatePath, publicPath } = routePaths;
+  const { user, loading } = useContext(AuthContext);
+  const { otherRoute, privatePath, publicPath, initialPath } = routePaths;
   const { auth, home } = publicPath;
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
 
   return (
     <Routes>
       {/* Public Routes  */}
       <Route path={home} element={<HomePage />} />
-      <Route
-        path={auth.path}
-        element={!user ? <AuthPage /> : <Navigate to={auth.navigateTo} />}
-      />
+      <Route path={auth.path} element={<AuthPage />} />
 
       {/* Private Routes */}
       <Route element={<ProtectedRoute />}>
-        <Route path={privatePath.initial} element={<MapPage />} />
+        <Route path={privatePath.map} element={<MapPage />} />
         <Route path={privatePath.profile} element={<Profile />} />
         <Route path={privatePath.password} element={<Password />} />
         <Route path={privatePath.deliveryman} element={<Deliveryman />} />
         <Route path={privatePath.branches} element={<Branches />} />
       </Route>
 
+      {/* Catch-all Route 
+        Redirect any unmatched paths to the initial path  */}
       <Route
         path={otherRoute.path}
         element={<Navigate to={otherRoute.navigateTo} />}
+      />
+
+      {/* Initial Route */}
+      {/* Redirects to /home if user is not authenticated, or to /map if authenticated */}
+      <Route
+        path={initialPath}
+        element={
+          !user ? <Navigate to={home} /> : <Navigate to={privatePath.map} />
+        }
       />
     </Routes>
   );
