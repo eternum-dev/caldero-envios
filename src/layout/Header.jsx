9f950e875b353f876952b3fc81a97916e2fda5a9
eternum-dev <alejandro.thon.j@gmail.com>
2024-12-12 10:59:07 -0300

    refactor(header): use link.path from headerData for navigation

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -4,11 +4,11 @@ import { headerData } from "../data";
 import "./header.css";
 
 export const Header = () => {
-  const { title } = headerData;
+  const { title, link } = headerData;
 
   return (
     <div className="header">
-      <Link to="/" className="header__title">
+      <Link to={link.path} className="header__title">
         <h1>{title.h1}</h1>
         <span>{title.span}</span>
       </Link>
