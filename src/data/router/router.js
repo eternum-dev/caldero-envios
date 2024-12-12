ac25c7b9fc72f8f75210d76d2859b1547335c7ed
eternum-dev <alejandro.thon.j@gmail.com>
2024-12-12 10:57:50 -0300

    feat(routes): add protectedRouteData to routePaths

--- src/data/router/router.js
+++ src/data/router/router.js
@@ -1,14 +1,19 @@
 export const routePaths = {
+  initialPath: "/",
   publicPath: {
     home: "/home",
     auth: { path: "/auth", navigateTo: "/" },
   },
   privatePath: {
-    initial: "/",
+    map: "/map",
     profile: "/configuracion/perfil",
     password: "/configuracion/contraseña",
     deliveryman: "/configuracion/repartidor",
     branches: "/configuracion/local",
   },
   otherRoute: { path: "/*", navigateTo: "/" },
+};
+
+export const protectedRouteData = {
+  path: "/home",
 };
