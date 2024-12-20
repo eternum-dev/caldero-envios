1621ab72a865d5872cd95f120fcd65ac3743de6c
eternum-dev <alejandro.thon.j@gmail.com>
2024-12-20 18:50:11 -0300

    chore: update exports in barrel file - sections added AboutCaldero, Membership, SinglePay

--- src/section/home/index.js
+++ src/section/home/index.js
@@ -1,2 +1,6 @@
+export * from './AboutCaldero';
 export * from './ImageContent';
 export * from './IntroContent';
+export * from './Membership';
+export * from './SinglePay';
+export * from './index';
