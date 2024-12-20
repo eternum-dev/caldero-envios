89f346b57d6b1f05ad642cbb579fa58b720d3289
eternum-dev <alejandro.thon.j@gmail.com>
2024-12-20 18:37:07 -0300

    feat(section): add  `Membership` component

---
+++ src/section/home/Membership.jsx
@@ -0,0 +1,13 @@
+import { PlanCard } from "../../components";
+
+export const Membership = () => {
+  return (
+    <>
+      <h2>Subcripcion</h2>
+      <section className="home-page__container membership">
+        <PlanCard />
+        <PlanCard />
+      </section>
+    </>
+  );
+};
