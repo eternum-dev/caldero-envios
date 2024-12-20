3ddfc25e279c3476af07f0fa8211b2609c7540c0
eternum-dev <alejandro.thon.j@gmail.com>
2024-12-20 18:41:31 -0300

    feat(home): update content and layout `HomePage` - Change the container start still label main. - add new sections `AboutCaldero`, `SinglePay`, `Membership`, `ContactMe`.

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,26 @@
-import { ImageContent, IntroContent } from "../../section/home";
+import { ContactMe } from "../../components/ContactMe";
+import {
+  AboutCaldero,
+  ImageContent,
+  IntroContent,
+  Membership,
+  SinglePay,
+} from "../../section/home";
 import "./homePage.css";
 
 export const HomePage = () => {
   return (
-    <div className="homePage">
-      <IntroContent />
-      <ImageContent />
-    </div>
+    <main className="homePage page">
+      <div className="home-page__content">
+        <IntroContent />
+        <ImageContent />
+      </div>
+      <AboutCaldero />
+      <div className="home-page__prices">
+        <SinglePay />
+        <Membership />
+      </div>
+      <ContactMe />
+    </main>
   );
 };
