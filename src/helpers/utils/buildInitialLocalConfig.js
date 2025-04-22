d6c6ed94a3ccb2b4078a1ab73952f8bce5044f2f
eternum-dev <alejandro.thon.j@gmail.com>
2025-04-22 17:06:17 -0400

    feat(setup): add buildInitialLocalConfig helper
    - Introduce `buildInitialLocalConfig(wizardData, user)` in `src/services/setupService.js`
    - Encapsulates logic for constructing the initial `repartidores`, `locales` and `user` payload for MapContext
    - Improves separation of concerns and makes setup logic more testable

---
+++ src/helpers/utils/buildInitialLocalConfig.js
@@ -0,0 +1,35 @@
+export const buildInitialLocalConfig = (wizardData, user) => {
+  const defaultUser =
+    "https://firebasestorage.googleapis.com/v0/b/caldero-envios.appspot.com/o/defaultUserPicture%2Fperfil.png?alt=media&token=a4d00b30-01c2-4919-ba02-f3801eb5a868";
+
+  return {
+    repartidores: [
+      {
+        nombre: wizardData.delivery.name || wizardData.name,
+        telefono: wizardData.delivery.phone || wizardData.phone,
+        metrics: [
+          {
+            distanceKilometers: wizardData.delivery.metrics.value,
+            totalCost: wizardData.delivery.metrics.valueDelivery,
+            unit: wizardData.delivery.metrics.unit,
+            type: "",
+          },
+        ],
+      },
+    ],
+    locales: [
+      {
+        numeroLocal: wizardData.localphone || wizardData.phone,
+        coordenadasLocal: wizardData.cord.cord,
+        nombreLocal: wizardData.localName,
+      },
+    ],
+    user: {
+      email: user.email,
+      name: wizardData.name,
+      profilePicture: defaultUser,
+      initialSetupCompleted: true,
+      country: wizardData.country,
+    },
+  };
+};
