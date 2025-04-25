f159a46811ea400acdcb437eeeac4c03db5975d9
eternum-dev <alejandro.thon.j@gmail.com>
2025-04-25 19:03:38 -0400

    refactor(helper): simplify getLocal function by removing unnecessary log

--- src/helpers/getLocal.js
+++ src/helpers/getLocal.js
@@ -3,13 +3,12 @@ import { auth, db } from "../firebase/firebase";
 
 export const getLocal = async () => {
   const user = auth.currentUser;
-  
+
   if (user) {
     const docRef = doc(db, "local", user.email);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-      console.log(docSnap.data());
       return docSnap.data();
     } else {
       return null;
