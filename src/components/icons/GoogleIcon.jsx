b2c51a05c0fcaa744771ecaa34cc1aefe139c484
eternum-dev <alejandro.thon.j@gmail.com>
2025-04-28 17:49:00 -0400

    feat(icons): add GoogleIcon component
    Created a new GoogleIcon component located at src/component/icons/GoogleIcon.jsx.

---
+++ src/components/icons/GoogleIcon.jsx
@@ -0,0 +1,24 @@
+export const GoogleIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="50"
+      height="50"
+      fill="none"
+      strokeWidth="1.5"
+      color="#ff8b8b"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#ff8b8b"
+        d="M15.548 8.303A5.15 5.15 0 0 0 12.108 7C9.288 7 7 9.239 7 12s2.287 5 5.109 5c3.47 0 4.751-2.57 4.891-4.583h-4.159"
+      ></path>
+      <path
+        stroke="#ff8b8b"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 8v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5"
+      ></path>
+    </svg>
+  );
+};
