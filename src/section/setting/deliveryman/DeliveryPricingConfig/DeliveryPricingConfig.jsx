2241f9674538a38c9f87be8dce34f6343f083e5f
eternum-dev <alejandro.thon.j@gmail.com>
2025-04-29 22:05:53 -0400

    feat(ui): add DeliveryPricingConfig component to handle delivery pricing modes

---
+++ src/section/setting/deliveryman/DeliveryPricingConfig/DeliveryPricingConfig.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { CheckBoxAdvanceMetrics } from "../../../../components/CheckBoxAdvanceMetrics";
+import { AdvancedPricing } from "./AdvancedPricing";
+import { SimplePricing } from "./SimplePricing";
+
+export const DeliveryPricingConfig = ({
+  wizardData,
+  setData,
+  showErrorsSection,
+  errors,
+}) => {
+  const unityMetrics = { kilometers: "Kilometros", meters: "Metros" };
+  const initialUnitMetrics =
+    wizardData?.delivery?.metrics || unityMetrics.meters;
+  const [unit, setUnit] = useState(initialUnitMetrics);
+  const data = [{ name: "", distanceKilometers: 0, totalCost: 0 }];
+  const [advanceMetrics, setAdvanceMetrics] = useState(false);
+  const [valueMetrics, setValueMetrics] = useState(0);
+
+  return (
+    <div className="valueroutes__wrapper">
+      <CheckBoxAdvanceMetrics
+        advanceMetrics={advanceMetrics}
+        setAdvanceMetrics={setAdvanceMetrics}
+      />
+      {advanceMetrics ? (
+        <AdvancedPricing
+          wizardData={wizardData}
+          data={data}
+          setData={setData}
+          setValueMetrics={setValueMetrics}
+          showErrorsSection={showErrorsSection}
+          errors={errors}
+          valueMetrics={valueMetrics}
+          unit={unit}
+          setUnit={setUnit}
+        />
+      ) : (
+        <SimplePricing
+          wizardData={wizardData}
+          data={data}
+          setData={setData}
+          setValueMetrics={setValueMetrics}
+          showErrorsSection={showErrorsSection}
+          errors={errors}
+          valueMetrics={valueMetrics}
+          unit={unit}
+          setUnit={setUnit}
+        />
+      )}
+    </div>
+  );
+};
+
+DeliveryPricingConfig.propTypes = {
+  wizardData: PropTypes.any,
+  setData: PropTypes.any,
+  showErrorsSection: PropTypes.any,
+  errors: PropTypes.any,
+};
