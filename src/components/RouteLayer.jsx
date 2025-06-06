af60939572f1a33ff46b8e1898aafbb2c309b09e
eternum-dev <alejandro.thon.j@gmail.com>
2025-06-06 18:38:52 -0400

    docs(routeLayer): add JSDoc and PropTypes to RouteLayer component

--- src/components/RouteLayer.jsx
+++ src/components/RouteLayer.jsx
@@ -1,5 +1,14 @@
 import { Source, Layer } from "react-map-gl/mapbox";
+import PropTypes from "prop-types";
 
+/**
+ * RouteLayer component renders a route on the map using Mapbox GL.
+ *
+ * @component
+ * @param {Object} props
+ * @param {Object} props.route - GeoJSON data representing the route geometry.
+ * @returns {JSX.Element|null} A route line layer if route is provided, otherwise null.
+ */
 
 export const RouteLayer = ({ route }) => {
   if (!route) return null;
@@ -23,3 +32,12 @@ export const RouteLayer = ({ route }) => {
   );
 };
 
+RouteLayer.propTypes = {
+  route: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    geometry: PropTypes.shape({
+      type: PropTypes.string.isRequired,
+      coordinates: PropTypes.arrayOf(PropTypes.array).isRequired,
+    }).isRequired,
+  }),
+};
