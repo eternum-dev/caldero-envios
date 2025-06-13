94974ae4efde6c7b90e23c034a8923090d91f2a5
eternum-dev <alejandro.thon.j@gmail.com>
2025-06-13 18:40:58 -0400

    refactor(formHeader): remove unused label for delivery values section

--- src/section/setting/deliveryman/FormHeader.jsx
+++ src/section/setting/deliveryman/FormHeader.jsx
@@ -2,13 +2,12 @@ import { CloseIcon } from "../../../components";
 import { deliveryRowHeader } from "../../../data";
 
 export const FormHeader = () => {
-  const { name, phoneNumber, values } = deliveryRowHeader;
+  const { name, phoneNumber } = deliveryRowHeader;
 
   return (
     <div className="delivery__row delivery__row--header">
       <span>{name}</span>
       <span>{phoneNumber}</span>
-      <span>{values}</span>
       <span>
         <CloseIcon height="24" width="24"/>
       </span>
